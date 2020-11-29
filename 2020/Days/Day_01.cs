3ff543da311964b1d8add5994b399f96f94a91a7
Julius F. Heins <jfheins@gmail.com>
2020-11-29 20:31:56 +0100

    Create solution and project for 2020

---
+++ 2020/Days/Day_01.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AoC_2020.Days
+{
+    public class Day_01 : BaseDay
+    {
+        private readonly string _input;
+
+        public Day_01()
+        {
+            _input = File.ReadAllText(InputFilePath);
+        }
+
+        public override string Solve_1() => $"Solution to {ClassPrefix} {CalculateIndex()}, part 1";
+
+        public override string Solve_2() => _input;
+    }
+}
---
+++ 2020/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using AoCHelper;
+
+namespace AoC_2020
+{
+    class Program
+    {
+        static void Main()
+        {
+            Solver.SolveAll();
+            _ = Console.ReadLine();
+        }
+    }
+}
