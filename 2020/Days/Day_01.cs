f69a189edf900984583c1401b8f1722317490454
Julius F. Heins <jfheins@gmail.com>
2020-12-01 07:00:47 +0100

    Day 1

--- 2020/Days/Day_01.cs
+++ 2020/Days/Day_01.cs
@@ -1,24 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+using Core;
 
 
 namespace AoC_2020.Days
 {
     public class Day_01 : BaseDay
     {
-        private readonly string _input;
+        private readonly int[] _input;
 
         public Day_01()
         {
-            _input = File.ReadAllText(InputFilePath);
+            _input = File.ReadAllLines(InputFilePath).Select(int.Parse).ToArray();
         }
 
-        public override string Solve_1() => $"Solution to {ClassPrefix} {CalculateIndex()}, part 1";
+        public override string Solve_1() => _input.CartesianProduct(_input).Where(x => x.Item1 + x.Item2 == 2020).Select(x => x.Item1 * x.Item2).First().ToString();
 
-        public override string Solve_2() => _input;
+        public override string Solve_2() => _input
+            .CartesianProduct(_input)
+            .CartesianProduct(_input)
+            .Select(x => (x.Item1.Item1, x.Item1.Item2, x.Item2))
+            .Where(x => x.Item1 + x.Item2 + x.Item3 == 2020)
+            .Select(x => x.Item1 * x.Item2 * x.Item3).First().ToString();
     }
 }
