a05d168bf5a0914a644bafde8914506eac0f0fcc
Julius F. Heins <jfheins@gmail.com>
2020-12-03 07:28:44 +0100

    Beautify day 3

--- 2020/Days/Day_03.cs
+++ 2020/Days/Day_03.cs
@@ -1,66 +1,47 @@
-﻿using System.Buffers;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Security.Cryptography.X509Certificates;
-using System.Text.RegularExpressions;
-
 
 using Core;
-using Core.Combinatorics;
 
 namespace AoC_2020.Days
 {
     public class Day_03 : BaseDay
     {
-        private readonly string[] _input;
+        private readonly WrappingXGrid2D<char> _input;
 
         public Day_03()
         {
-            _input = File.ReadAllLines(InputFilePath);
+            _input = new WrappingXGrid2D<char>(File.ReadAllLines(InputFilePath));
         }
 
         public override string Solve_1()
         {
-            int treecount = Calc(1, 3);
-            return treecount.ToString();
+            return CountTreesInSlope(3, 1).ToString();
         }
 
-        private int Calc(int dy, int dx)
+        private int CountTreesInSlope(int dx, int dy)
         {
-            var x = 0;
-            var maxy = _input.Length - 1;
-            var treecount = 0;
-            for (int y = 0; y <= maxy; y += dy)
-            {
-                if (_input[y][x] == '#')
-                {
-                    treecount++;
-                }
-                x = (x + dx) % _input[0].Length;
-            }
-
-            return treecount;
+            var origin = new Point();
+            var delta = new Size(dx, dy);
+            return Enumerable.Range(0, int.MaxValue)
+                .Select(x => origin + (x * delta))
+                .TakeWhile(_input.Contains)
+                .Count(p => _input[p] == '#');
         }
 
         public override string Solve_2()
         {
-            //            Right 1, down 1.
-            //Right 3, down 1. (This is the slope you already checked.)
-            //Right 5, down 1.
-            //Right 7, down 1.
-            //Right 1, down 2.
-            var res = new List<long>
+            var paths = new List<long>
             {
-                Calc(1, 1),
-                Calc(1, 3),
-                Calc(1, 5),
-                Calc(1, 7),
-                Calc(2, 1)
+                CountTreesInSlope(1, 1),
+                CountTreesInSlope(3, 1),
+                CountTreesInSlope(5, 1),
+                CountTreesInSlope(7, 1),
+                CountTreesInSlope(1, 2)
             };
-            return res.Product().ToString();
+            return paths.Product().ToString();
         }
     }
 }
