ad5e3b6f43098e5c60bb24ae6da5c9b062cbb0e2
Julius F. Heins <jfheins@gmail.com>
2020-12-06 06:39:09 +0100

    Day 5

---
+++ 2020/Days/Day_05.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+using MoreLinq;
+
+namespace AoC_2020.Days
+{
+    public class Day_05 : BaseDay
+    {
+        private readonly string[] _input;
+
+        public Day_05()
+        {
+            _input = File.ReadAllLines(InputFilePath);
+        }
+
+        public override string Solve_1()
+        {
+            var binary = _input.Select(s => s.Replace("R", "1").Replace("B", "1").Replace('F', '0').Replace('L', '0'));
+            var seatIds = binary.Select(x => Convert.ToInt32(x, 2));
+            return seatIds.Max().ToString();
+        }
+
+        public override string Solve_2()
+        {
+            var binary = _input.Select(s => s.Replace("R", "1").Replace("B", "1").Replace('F', '0').Replace('L', '0'));
+            var seatIds = binary.Select(x => Convert.ToInt32(x, 2)).OrderBy(x => x).ToList();
+            var offset = seatIds.First();
+            return (seatIds.Where((seatId, idx) => idx + offset != seatId).First() - 1).ToString();
+        }
+    }
+}
