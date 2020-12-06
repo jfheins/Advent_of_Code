924305d84ec5d7a580846b371d72bf8838c8fb22
Julius F. Heins <jfheins@gmail.com>
2020-12-06 06:39:23 +0100

    Day 6

---
+++ 2020/Days/Day_06.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+using MoreLinq;
+
+namespace AoC_2020.Days
+{
+    public class Day_06 : BaseDay
+    {
+        private readonly List<IEnumerable<string>> _input;
+
+        public Day_06()
+        {
+            _input = File.ReadAllLines(InputFilePath).Split("").ToList();
+        }
+
+        public override string Solve_1()
+        {
+            return _input.Select(block => string.Concat(block).Distinct().Count()).Sum().ToString();
+        }
+
+        public override string Solve_2()
+        {
+            return _input.Select(block => block.Aggregate((set, elem) => string.Concat(set.Intersect(elem))).Length).Sum().ToString();
+        }
+    }
+}
