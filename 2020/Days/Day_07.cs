4f65502b0815263716cb4f4880e06b88af0431a0
Julius F. Heins <jfheins@gmail.com>
2020-12-07 09:22:43 +0100

    Beautify day 7

--- 2020/Days/Day_07.cs
+++ 2020/Days/Day_07.cs
@@ -10,75 +10,60 @@ namespace AoC_2020.Days
 {
     public class Day_07 : BaseDay
     {
-        private readonly string[] _input;
+        private Dictionary<string, (string color, int count)[]> contentsOf = new();
+        private Dictionary<string, List<string>> containersOf = new();
+
+        private readonly string[] input;
 
         public Day_07()
         {
-            _input = File.ReadAllLines(InputFilePath);
+            input = File.ReadAllLines(InputFilePath);
         }
 
         public override string Solve_1()
         {
-            var bags = _input.Select(ParseBag).ToList();
+            var containerRegex = new Regex(@"(\w+ \w+) bags contain");
+            var contentRegex = new Regex(@"(\d) (\w+ \w+) bag");
 
-            var possibleContainers = bags.Where(b => b.Item2.Contains("shiny gold")).Select(x => x.Item1).ToHashSet();
-
-            for (int i = 0; i < 1000; i++)
+            foreach (var line in input)
             {
-                var add = bags.Where(b => b.Item2.Any(bag => possibleContainers.Contains(bag))).Select(x => x.Item1);
-                possibleContainers.UnionWith(add);
+                var container = containerRegex.Match(line).Groups[1].Value;
+                var content = contentRegex.Matches(line).Select(match => (color: match.Groups[2].Value, count: int.Parse(match.Groups[1].Value))).ToArray();
+
+                contentsOf[container] = content;
+                foreach (var (color, count) in content)
+                    if (containersOf.TryGetValue(color, out var val))
+                        val.Add(container);
+                    else
+                        containersOf[color] = new List<string> { container };
             }
-            return "";
-        }
 
-        private (string, string[]) ParseBag(string b)
-        {
-            var container = new Regex(@"(\w+ \w+) bags contain");
-            var content = new Regex(@"\d (\w+ \w+) bag");
+            var emptyList = new List<string>();
+            var search = new BreadthFirstSearch<string>(StringComparer.Ordinal, bag => containersOf.GetValueOrDefault(bag, emptyList));
 
-            var c = container.Match(b).Groups[1].Value;
-            var cn = content.Matches(b).Select(match => match.Groups[1].Value).ToArray();
-
-            return (c, cn);
-        }
-
-        private (string, (int count, string color)[]) ParseBag2(string b)
-        {
-            var container = new Regex(@"(\w+ \w+) bags contain");
-            var content = new Regex(@"(\d) (\w+ \w+) bag");
-
-            var c = container.Match(b).Groups[1].Value;
-            var cn = content.Matches(b).Select(match => (int.Parse(match.Groups[1].Value), match.Groups[2].Value)).ToArray();
-
-            return (c, cn);
+            return search.FindAll("shiny gold", bag => bag != "shiny gold").Count.ToString();
         }
 
         public override string Solve_2()
         {
-            var bags = _input.Select(ParseBag2).ToDictionary(t => t.Item1);
             var content = new Dictionary<string, int>();
             var newcontent = new Dictionary<string, int>();
 
-            foreach (var (count, color) in bags["shiny gold"].Item2)
-            {
+            foreach (var (color, count) in contentsOf["shiny gold"])
                 newcontent.Add(color, count);
-            }
 
-
-            for (int i = 0; i < 1000; i++)
+            while (newcontent.Any())
             {
                 foreach (var kvp in newcontent)
                     content.AddOrModify(kvp.Key, 0, c => c + kvp.Value);
 
-                var toadd = newcontent.SelectMany(bag => bags[bag.Key].Item2.Select(child => new { parentcount = bag.Value, child })).ToList();
-                newcontent = new Dictionary<string, int>();
-                foreach (var newBag in toadd)
-                {
-                    newcontent.AddOrModify(newBag.child.color, 0, c => c + newBag.parentcount * newBag.child.count);
-                }
+                var nextItems = newcontent.SelectMany(bag => contentsOf[bag.Key].Select(child => (child.color, count: bag.Value * child.count))).ToList();
+                newcontent.Clear();
+                foreach (var (color, count) in nextItems)
+                    newcontent.AddOrModify(color, 0, c => c + count);
             }
 
-            return content.Sum(x => x.Value).ToString();
+            return content.Values.Sum().ToString();
         }
     }
 }
