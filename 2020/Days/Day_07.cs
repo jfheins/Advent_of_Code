e26fee434804a82cc4b890f2143116d9ddd5fe0e
Julius F. Heins <jfheins@gmail.com>
2020-12-07 13:52:23 +0100

    Move some parsing back into ctor

--- 2020/Days/Day_07.cs
+++ 2020/Days/Day_07.cs
@@ -10,18 +10,13 @@ namespace AoC_2020.Days
 {
     public class Day_07 : BaseDay
     {
-        private Dictionary<string, (string color, int count)[]> contentsOf = new();
-        private Dictionary<string, List<string>> containersOf = new();
-
-        private readonly string[] input;
+        private readonly Dictionary<string, (string color, int count)[]> contentsOf = new();
+        private readonly Dictionary<string, List<string>> containersOf = new();
 
         public Day_07()
         {
-            input = File.ReadAllLines(InputFilePath);
-        }
+            var input = File.ReadAllLines(InputFilePath);
 
-        public override string Solve_1()
-        {
             var containerRegex = new Regex(@"(\w+ \w+) bags contain");
             var contentRegex = new Regex(@"(\d) (\w+ \w+) bag");
 
@@ -37,10 +32,15 @@ namespace AoC_2020.Days
                     else
                         containersOf[color] = new List<string> { container };
             }
+        }
 
+        public override string Solve_1()
+        {
             var emptyList = new List<string>();
-            var search = new BreadthFirstSearch<string>(StringComparer.Ordinal, bag => containersOf.GetValueOrDefault(bag, emptyList));
-
+            var search = new BreadthFirstSearch<string>(StringComparer.Ordinal, bag => containersOf.GetValueOrDefault(bag, emptyList))
+            {
+                PerformParallelSearch = false
+            };
             return search.FindAll("shiny gold", bag => bag != "shiny gold").Count.ToString();
         }
 
@@ -57,7 +57,9 @@ namespace AoC_2020.Days
                 foreach (var kvp in newcontent)
                     content.AddOrModify(kvp.Key, 0, c => c + kvp.Value);
 
-                var nextItems = newcontent.SelectMany(bag => contentsOf[bag.Key].Select(child => (child.color, count: bag.Value * child.count))).ToList();
+                var nextItems = newcontent
+                    .SelectMany(bag => contentsOf[bag.Key].Select(child => (child.color, count: bag.Value * child.count)))
+                    .ToList();
                 newcontent.Clear();
                 foreach (var (color, count) in nextItems)
                     newcontent.AddOrModify(color, 0, c => c + count);
