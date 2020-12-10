15dfb9b4cb1e429f97a4338d03992d5e04d8c049
Julius F. Heins <jfheins@gmail.com>
2020-12-10 07:58:12 +0100

    Beautify day 10

--- 2020/Days/Day_10.cs
+++ 2020/Days/Day_10.cs
@@ -11,50 +11,52 @@ namespace AoC_2020.Days
 {
     public class Day_10 : BaseDay
     {
-        private readonly long[] inputNum;
-        private readonly string[] input;
+        private readonly int[] input;
 
         public Day_10()
         {
-            inputNum = File.ReadAllLines(InputFilePath).Select(long.Parse).ToArray();
-            input = File.ReadAllLines(InputFilePath);
+            input = File.ReadAllLines(InputFilePath).Select(int.Parse).ToArray();
         }
 
         public override string Solve_1()
         {
-            var device = inputNum.Max() + 3;
-            var c = inputNum
+            var device = input.Max() + 3;
+            var diffs = input
                 .Append(device)
                 .Append(0)
                 .OrderBy(x => x)
-                .PairwiseWithOverlap()
-                .Select(x => x.Diff()).ToList();
+                .Diff()
+                .ToList();
 
-            return (c.Count(x => x == 1) * c.Count(x => x == 3)).ToString();
+            return (diffs.Count(x => x == 1) * diffs.Count(x => x == 3)).ToString();
         }
 
         public override string Solve_2()
         {
-            var device = inputNum.Max() + 3;
-            var sorted = inputNum
+            var device = input.Max() + 3;
+
+            var diffs = input
                 .Append(device)
                 .Append(0)
                 .OrderBy(x => x)
-                .PairwiseWithOverlap()
-                .Select(x => x.Diff()).ToList();
+                .Diff()
+                .ToList();
 
-            var runs = sorted.Chunks().Where(c => c[0] == 1).Select(c => c.Count)
-                .Select(len =>
-            len switch
-            {
-                1 => 1L,
-                2 => 2L,
-                3 => 4L,
-                4 => 7L,
-                _ => 0L
-            }).Product();
-
-            return runs.ToString();
+            return diffs
+                .Chunks()
+                .Where(c => c[0] == 1)
+                .Select(c => c.Count)
+                .Select(runlength =>
+                    runlength switch
+                    {
+                        1 => 1L,
+                        2 => 2L,
+                        3 => 4L,
+                        4 => 7L,
+                        _ => 0L
+                    })
+                .Product()
+                .ToString();
         }
     }
 }
