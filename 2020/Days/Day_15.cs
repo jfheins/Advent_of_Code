ef51885fd3e5ddcc69a92eb9b52b3153f60f3776
Julius F. Heins <jfheins@gmail.com>
2020-12-15 08:16:01 +0100

    Improve performance

--- 2020/Days/Day_15.cs
+++ 2020/Days/Day_15.cs
@@ -12,15 +12,11 @@ namespace AoC_2020.Days
 {
     public class Day_15 : BaseDay
     {
-        private readonly string input;
         private readonly int[] numbers;
-        //private FiniteGrid2D<char> grid;
 
         public Day_15()
         {
-            input = File.ReadAllText(InputFilePath);
-            numbers = input.ParseInts();
-            //grid = Grid2D.FromFile(InputFilePath);
+            numbers = File.ReadAllText(InputFilePath).ParseInts();
         }
 
         public override string Solve_1()
@@ -45,37 +41,25 @@ namespace AoC_2020.Days
 
         public override string Solve_2()
         {
-            var spoken = new List<int>(30000000);
-            spoken.AddRange(numbers);
+            var mapNumToTurn = new Dictionary<int, int>(4_000_000);
+            var prevNum = -1;
 
-            var mapNumToTurn = new Dictionary<int, List<int>>();
-
-            for (int i = 0; i < spoken.Count; i++)
+            for (int i = 0; i < numbers.Length; i++)
             {
-                var list = mapNumToTurn.GetOrAdd(spoken[i], new List<int>());
-                list.Add(i);
+                mapNumToTurn[numbers[i]] = i;
+                prevNum = numbers[i];
             }
 
             for (int i = numbers.Length; i < 30000000; i++)
             {
-                var mostrecent = spoken[i - 1];
-                var idx = mapNumToTurn.GetValueOrDefault(mostrecent, new List<int>());
-                var wasBefore = idx.Count > 1;
-                if (wasBefore)
-                {
-                    var newNumber = idx[^1] - idx[^2];
-                    spoken.Add(newNumber);
-                    var list = mapNumToTurn.GetOrAdd(newNumber, new List<int>());
-                    list.Add(i);
-                }
-                else
-                {
-                    spoken.Add(0);
-                    var list = mapNumToTurn.GetOrAdd(0, new List<int>());
-                    list.Add(i);
-                }
+                var newNumber = 0;
+                if (mapNumToTurn.TryGetValue(prevNum, out var idx))
+                    newNumber = i - 1 - idx;
+                
+                mapNumToTurn[prevNum] = i-1;
+                prevNum = newNumber;
             }
-            return spoken.Last().ToString();
+            return prevNum.ToString();
         }
     }
 }
