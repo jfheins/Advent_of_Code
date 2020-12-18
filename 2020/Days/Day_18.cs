65963b73550cd1155e603f4b18e87360e712e987
Julius F. Heins <jfheins@gmail.com>
2020-12-18 07:21:11 +0100

    Beautify day 18

--- 2020/Days/Day_18.cs
+++ 2020/Days/Day_18.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Globalization;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -13,29 +9,58 @@ namespace AoC_2020.Days
     public class Day_18 : BaseDay
     {
         private readonly string[] input;
-        //private readonly int[] numbers;
-        //private FiniteGrid2D<char> grid;
 
         public Day_18()
         {
             input = File.ReadAllLines(InputFilePath);
-            //numbers = input.Select(int.Parse).ToArray();
-            //grid = Grid2D.FromFile(InputFilePath);
         }
 
         public override string Solve_1()
         {
-            var sum = 0L;
-            foreach (var line in input)
-            {
-                sum += long.Parse(Eval(line));
-            }
-            
-            return sum.ToString();
+            return input
+                .Select(line => long.Parse(
+                    EvalLeftToRight(line)))
+                .Sum().ToString();
         }
 
-        string EvalMatch(Match match) => Eval(match.Value[1..^1]);
-        string EvalBaseMatch(Match match)
+
+        public override string Solve_2()
+        {
+            return input
+                .Select(line => long.Parse(
+                    EvalAddBeforeMul(line)))
+                .Sum().ToString();
+        }
+
+        private string EvalLeftToRight(string expression)
+            => Eval(expression, new Regex(@"^(-?\d+) ([+*]) (-?\d+)"));
+
+        private string EvalAddBeforeMul(string expression)
+        {
+            return Eval(expression,
+                new Regex(@"(-?\d+) ([+]) (-?\d+)"),
+                new Regex(@"(-?\d+) ([*]) (-?\d+)"));
+        }
+
+        private string Eval(string expression, params Regex[] operationsInOrder)
+        {
+            string EvalParenthesis(Match match) => Eval(match.Value[1..^1], operationsInOrder);
+
+            var parens = new Regex(@"\([^()]+\)");
+            while (parens.IsMatch(expression))
+                expression = parens.Replace(expression, EvalParenthesis);
+
+            foreach (var regex in operationsInOrder)
+            {
+                while (regex.IsMatch(expression))
+                {
+                    expression = regex.Replace(expression, EvalSingleOperation);
+                }
+            }
+            return expression;
+        }
+
+        string EvalSingleOperation(Match match)
         {
             var operands = match.Value.ParseLongs();
             var op = match.Groups[2].Value;
@@ -45,37 +70,6 @@ namespace AoC_2020.Days
                 "*" => operands.Product().ToString(),
                 _ => "x"
             };
-        }
-
-        private string Eval(string expression)
-        {
-            var parens = new Regex(@"\([^()]+\)");
-            var match = parens.Match(expression);
-            while (parens.IsMatch(expression))
-            {
-                expression = parens.Replace(expression, EvalMatch);
-            }
-            var op = new Regex(@"(-?\d+) ([+]) (-?\d+)");
-
-            while (op.IsMatch(expression))
-            {
-                expression = op.Replace(expression, EvalBaseMatch);
-            }
-
-            op = new Regex(@"(-?\d+) ([*]) (-?\d+)");
-
-            while (op.IsMatch(expression))
-            {
-                expression = op.Replace(expression, EvalBaseMatch);
-            }
-            return expression;
-        }
-
-
-        public override string Solve_2()
-        {
-
-            return "_";
         }
     }
 }
