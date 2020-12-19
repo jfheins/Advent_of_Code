788edf73041a620333f0cc4fcdda9e10747ce85c
Julius F. Heins <jfheins@gmail.com>
2020-12-19 09:05:53 +0100

    Beautify day 19

--- 2020/Days/Day_19.cs
+++ 2020/Days/Day_19.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Globalization;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection.Metadata.Ecma335;
 using System.Text.RegularExpressions;
 
 using Core;
@@ -15,59 +11,29 @@ namespace AoC_2020.Days
 {
     public class Day_19 : BaseDay
     {
-        private readonly string[] input;
-        private readonly int[] numbers;
-        private Dictionary<int, string> cache = new();
-
-        //private FiniteGrid2D<char> grid;
+        private readonly Dictionary<int, string> cache = new();
+        private readonly string[] rules;
+        private readonly string[] messages;
+        private Dictionary<int, string> ruleDict;
+        private long time;
 
         public Day_19()
         {
-            input = File.ReadAllLines(InputFilePath);
-            //numbers = input.Select(int.Parse).ToArray();
-            //grid = Grid2D.FromFile(InputFilePath);
+            var input = File.ReadAllLines(InputFilePath).Split("").ToArray();
+            rules = input[0].Select(l => l.Replace("\"", "")).ToArray();
+            messages = input[1].ToArray();
         }
 
         public override string Solve_1()
         {
-            var blocks = input.Split("").Select(x => x.ToArray()).ToArray();
-            var rules = blocks[0].ToDictionary(s => int.Parse(s.Split(":")[0]), s => s.Split(":")[1].Replace("\"", ""));
-
-            var regex = Resolve(rules, 0);
-            var x = new Regex("^" + regex.Replace(" ", "") + "$", RegexOptions.Compiled);
-
-            var count = 0;
-            for (int i = 0; i < blocks[1].Length; i++)
+            static KeyValuePair<int, string> ParseRule(string rule)
             {
-                var line = blocks[1][i];
-                if (x.IsMatch(line))
-                {
-                    count++;
-                    Console.WriteLine($"{i} / {blocks[1].Length}");
-                }
+                var parts = rule.Split(": ");
+                return KeyValuePair.Create(int.Parse(parts[0]), parts[1]);
             }
-
-            return blocks[1].Count(line => x.IsMatch(line)).ToString();
-        }
-
-        private string Resolve(Dictionary<int, string> rules, int index)
-        {
-            var value = rules[index];
-            var orig = value;
-            var addparens = value.Contains("|");
-            if (!value.Any(c => char.IsDigit(c)))
-            {
-                return value;
-            }
-            var links = value.ParseInts();
-            var repl = links.Distinct().ToDictionary(link => link, link => cache.GetOrAdd(link, i => Resolve(rules, i)));
-            value = Regex.Replace(value, @"\d+", m => repl[int.Parse(m.Value)]);
-
-            if (addparens)
-            {
-                value = "(" + value + ")";
-            }
-            return value.Replace(" ", "");
+            ruleDict = new Dictionary<int, string>(rules.Select(ParseRule));
+            var regex = new Regex("^" + Resolve(0) + "$", RegexOptions.ExplicitCapture);
+            return messages.Count(line => regex.IsMatch(line)).ToString();
         }
 
         public override string Solve_2()
@@ -75,44 +41,32 @@ namespace AoC_2020.Days
             // 0: 8 11
             // 8: 42 | 42 8
             // 11: 42 31 | 42 11 31
+            // => 42*m 42*n 31*n where m, n > 0
 
-            var blocks = input.Split("").Select(x => x.ToArray()).ToArray();
-            var rules = blocks[0].ToDictionary(s => int.Parse(s.Split(":")[0]), s => s.Split(":")[1].Replace("\"", ""));
+            var p42 = Resolve(42);
+            var p31 = Resolve(31);
+            var overall = new Regex(@$"^{p42}+(?'n'{p42})+(?'-n'{p31})+(?(n)(?!))$", RegexOptions.ExplicitCapture);
+            return messages.Count(line => overall.IsMatch(line)).ToString();
+        }
 
-            var overall = new Regex("^(" + Resolve(rules, 42) + ")+(" + Resolve(rules, 31) + ")+$", RegexOptions.Compiled);
-            var regex42 = new Regex(Resolve(rules, 42), RegexOptions.Compiled);
-            var regex42start = new Regex("^" + Resolve(rules, 42), RegexOptions.Compiled);
-            var regex31 = new Regex(Resolve(rules, 31), RegexOptions.Compiled);
-            var regex31start = new Regex("^" + Resolve(rules, 31), RegexOptions.Compiled);
-
-            var count = 0;
-            for (int i = 0; i < blocks[1].Length; i++)
+        private string Resolve(int index)
+        {
+            var value = ruleDict[index];
+            var addparens = value.Contains("|");
+            if (!value.Any(c => char.IsDigit(c)))
             {
-                var line = blocks[1][i];
-                var start42 = 0;
-                while (regex42start.IsMatch(line))
-                {
-                    start42++;
-                    line = regex42start.Replace(line, "");
-                }
-
-                var end31 = 0;
-                while (regex31start.IsMatch(line))
-                {
-                    end31++;
-                    line = regex31start.Replace(line, "");
-                }
-                if (line.Length > 0)
-                    continue;
-                
-                if (end31 > 0 && end31 < start42)
-                {
-                    count++;
-                    Console.WriteLine($"{i} / {blocks[1].Length}");
-                }
+                return value;
             }
+            var links = value.ParseInts();
+            var repl = links.Distinct()
+                .ToDictionary(link => link, link => cache.GetOrAdd(link, i => Resolve(i)));
+            value = Regex.Replace(value, @"\d+", m => repl[int.Parse(m.Value)]);
 
-            return count.ToString();
+            if (addparens)
+            {
+                value = "(" + value + ")";
+            }
+            return value.Replace(" ", "");
         }
     }
 }
