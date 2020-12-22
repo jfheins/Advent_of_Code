ac510932913d736c6c3f4cef0f67ff799bfc2c9b
Julius F. Heins <jfheins@gmail.com>
2020-12-22 19:55:00 +0100

    Use immutable array for better performance

--- 2020/Days/Day_22.cs
+++ 2020/Days/Day_22.cs
@@ -19,21 +19,21 @@ namespace AoC_2020.Days
     {
         private readonly LinkedList<int> player1;
         private readonly LinkedList<int> player2;
-        private ImmutableList<int> p1list;
-        private ImmutableList<int> p2list;
+        private ImmutableArray<int> p1list;
+        private ImmutableArray<int> p2list;
 
         public Day_22()
         {
             var input = File.ReadAllLines(InputFilePath).Split("").ToArray(); ;
             player1 = new LinkedList<int>(input[0].Skip(1).Select(int.Parse));
             player2 = new LinkedList<int>(input[1].Skip(1).Select(int.Parse));
-            p1list = player1.ToImmutableList();
-            p2list = player2.ToImmutableList();
+            p1list = player1.ToImmutableArray();
+            p2list = player2.ToImmutableArray();
         }
 
         public override string Solve_1()
         {
-            while (player1.Count * player2.Count > 0)
+            while (player1.Any() && player2.Any())
             {
                 var played = (player1.First!.Value, player2.First!.Value);
 
@@ -62,14 +62,13 @@ namespace AoC_2020.Days
         public override string Solve_2()
         {
             var (_, deck) = RecurseCombat(p1list, p2list);
-            var count = deck.Count;
-            var score = deck.Select((item, idx) => (count - idx) * (long)item).Sum();
+            var score = deck.Select((item, idx) => (deck.Length - idx) * (long)item).Sum();
             return score.ToString();
         }
 
-        private (int player, ImmutableList<int> deck) RecurseCombat(ImmutableList<int> deck1, ImmutableList<int> deck2)
+        private (int player, ImmutableArray<int> deck) RecurseCombat(ImmutableArray<int> deck1, ImmutableArray<int> deck2, int depth = 1)
         {
-            HashSet<(ImmutableList<int>, ImmutableList<int>)> seen = new(new SequenceComparer());
+            HashSet<(ImmutableArray<int>, ImmutableArray<int>)> seen = new(new SequenceComparer());
             var drawn = new int[2];
             while (deck1.Any() && deck2.Any())
             {
@@ -78,38 +77,38 @@ namespace AoC_2020.Days
 
                 drawn[0] = deck1[0];
                 drawn[1] = deck2[0];
+                deck1 = deck1.RemoveAt(0);
+                deck2 = deck2.RemoveAt(0);
 
                 int winner;
-                if (deck1.Count > drawn[0] && deck2.Count > drawn[1])
-                    winner = RecurseCombat(deck1.GetRange(1, drawn[0]), deck2.GetRange(1, drawn[1])).player;
+                if (deck1.Length >= drawn[0] && deck2.Length >= drawn[1])
+                    winner = RecurseCombat(ImmutableArray.CreateRange(deck1.Take(drawn[0])), ImmutableArray.CreateRange(deck2.Take(drawn[1])), depth + 1).player;
                 else
                     winner = drawn[0] > drawn[1] ? 1 : 2;
 
                 if (winner == 1)
                 {
-                    deck1 = deck1.RemoveAt(0).AddRange(drawn);
-                    deck2 = deck2.RemoveAt(0);
+                    deck1 = deck1.AddRange(drawn);
                 }
-                else if(winner == 2)
+                else if (winner == 2)
                 {
-                    deck1 = deck1.RemoveAt(0);
-                    deck2 = deck2.RemoveAt(0).AddRange(drawn.Reverse());
+                    deck2 = deck2.AddRange(drawn.Reverse());
                 }
             }
             return deck1.Any() ? (1, deck1) : (2, deck2);
         }
 
-        private class SequenceComparer : IEqualityComparer<(ImmutableList<int>, ImmutableList<int>)>
+        private class SequenceComparer : IEqualityComparer<(ImmutableArray<int>, ImmutableArray<int>)>
         {
-            public bool Equals((ImmutableList<int>, ImmutableList<int>) x, (ImmutableList<int>, ImmutableList<int>) y)
+            public bool Equals((ImmutableArray<int>, ImmutableArray<int>) x, (ImmutableArray<int>, ImmutableArray<int>) y)
             {
                 return Enumerable.SequenceEqual(x.Item1, y.Item1) && Enumerable.SequenceEqual(x.Item2, y.Item2);
             }
 
-            public int GetHashCode([DisallowNull] (ImmutableList<int>, ImmutableList<int>) obj)
+            public int GetHashCode([DisallowNull] (ImmutableArray<int>, ImmutableArray<int>) obj)
             {
-                var left = obj.Item1.Count > 2 ? obj.Item1[0] + (obj.Item1[1] * 59) + (obj.Item1[^1] * 59 * 59) : 487;
-                var right = obj.Item2.Count > 2 ? unchecked((obj.Item2[0] + (obj.Item2[1] * 59) + (obj.Item2[^1] * 59 * 59)) << 20) : 1000037;
+                var left = obj.Item1.Length > 2 ? obj.Item1[0] + (obj.Item1[1] * 59) + (obj.Item1[^1] * 59 * 59) : 487;
+                var right = obj.Item2.Length > 2 ? unchecked((obj.Item2[0] + (obj.Item2[1] * 59) + (obj.Item2[^1] * 59 * 59)) << 20) : 1000037;
                 return left ^ right;
             }
         }
