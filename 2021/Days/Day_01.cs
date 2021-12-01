706ce9f86299ad6446535e08b2a9772ba580ef95
Julius Heins <Julius.JH.Heins@bmw.de>
2021-12-01 13:44:58 +0100

    Beautify

--- 2021/Days/Day_01.cs
+++ 2021/Days/Day_01.cs
@@ -17,12 +17,15 @@ namespace AoC_2021.Days
             _input = File.ReadAllLines(InputFilePath).Select(int.Parse).ToArray();
         }
 
-        public override async ValueTask<string> Solve_1() => _input.Diff()
-            .Count(x => x > 0).ToString();
+        public override async ValueTask<string> Solve_1()
+            => _input.Diff().Count(x => x > 0).ToString();
 
-        public override async ValueTask<string> Solve_2() => _input.Window(3)
-            .Select(x => x.Sum())
-            .Diff()
-            .Count(x => x > 0).ToString();
+        public override async ValueTask<string> Solve_2()
+        {
+            return _input.Window(3)
+                       .Select(x => x.Sum())
+                       .Diff()
+                       .Count(x => x > 0).ToString();
+        }
     }
 }
