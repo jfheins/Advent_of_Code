a0fb2c92377778fa30a2796aba66b7c05987fe8e
Julius Heins <Julius.JH.Heins@bmw.de>
2021-12-01 16:28:57 +0100

    Solve day 3

--- 2015/Days/Day_02.cs
+++ 2015/Days/Day_02.cs
@@ -17,19 +17,22 @@ namespace AoC_2015.Days
             _input = File.ReadAllLines(InputFilePath).Select(line => line.ParseNNInts(3)).ToList();
         }
 
-        private IEnumerable<int> FaceAreas(int[] s)
+        private static IEnumerable<int> ShiftLeft(IEnumerable<int> source)
         {
-            yield return s[0] * s[1];
-            yield return s[1] * s[2];
-            yield return s[2] * s[0];
+            return source.Skip(1).Append(source.First());
         }
 
-        private IEnumerable<int> FacePerimeters(int[] s)
+        private IEnumerable<(int, int)> ZipWithNextWraparound(IEnumerable<int> source)
         {
-            yield return 2 * (s[0] + s[1]);
-            yield return 2 * (s[1] + s[2]);
-            yield return 2 * (s[2] + s[0]);
+            var all = source.ToList();
+            return all.Select((x, i) => (x, all[(i + 1) % all.Count]));
         }
+
+        private IEnumerable<int> FaceAreas(int[] s)
+            => s.Zip(ShiftLeft(s)).Select(t => t.First * t.Second);
+
+        private IEnumerable<int> FacePerimeters(int[] s)
+            => s.Zip(ShiftLeft(s)).Select(t => 2* (t.First + t.Second));
 
         private int CalculateArea(int[] s) => 2 * FaceAreas(s).Sum();
         private int SmallestFace(int[] s) => FaceAreas(s).Min();
@@ -38,12 +41,15 @@ namespace AoC_2015.Days
 
         public override async ValueTask<string> Solve_1()
         {
-            return _input.Select(gift => CalculateArea(gift) + SmallestFace(gift)).Sum().ToString();
+            return _input
+                .Select(gift => CalculateArea(gift) + SmallestFace(gift)).Sum().ToString();
         }
 
         public override async ValueTask<string> Solve_2()
         {
-            return _input.Select(gift => FacePerimeters(gift).Min() + Volume(gift)).Sum().ToString();
+            var xx = ShiftLeft(FaceAreas(_input[0])).ToList();
+            return _input
+                .Select(gift => FacePerimeters(gift).Min() + Volume(gift)).Sum().ToString();
         }
     }
 }
---
+++ 2015/Days/Day_03.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Immutable;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+using Core;
+using Core.Combinatorics;
+
+using MoreLinq.Extensions;
+
+namespace AoC_2015.Days
+{
+    public class Day_03 : BaseDay
+    {
+        private readonly Direction[] _input;
+
+        public Day_03()
+        {
+            _input = File.ReadAllText(InputFilePath).Select(c => c switch
+            {
+                '>' => Direction.Right,
+                '^' => Direction.Up,
+                '<' => Direction.Left,
+                'v' => Direction.Down,
+            }).ToArray();
+        }
+
+        public override async ValueTask<string> Solve_1()
+        {
+            var currentPos = new Point(0, 0);
+            var positions = new List<Point> { currentPos };
+
+            foreach (var dir in _input)
+            {
+                currentPos += dir.ToSize();
+                positions.Add(currentPos);
+            }
+            return new HashSet<Point>(positions).Count.ToString();
+        }
+
+        public override async ValueTask<string> Solve_2()
+        {
+            var currentPos = new Point(0, 0);
+            var positions = new List<Point> { currentPos };
+
+            var emptyList = ImmutableList.Create(new Point(0, 0));
+
+            // Funktional, aber schlechte performance
+            var santasHouses = _input.StepBy(2)
+                .Aggregate(emptyList, (list, dir) => list.Add(list.Last() + dir.ToSize()));
+
+            var roboSantasHouses = _input.StepBy(2, 1)
+                .Aggregate(emptyList, (list, dir) => list.Add(list.Last() + dir.ToSize()));
+
+            var allHouses = new HashSet<Point>(santasHouses);
+            allHouses.UnionWith(roboSantasHouses);
+
+            return allHouses.Count.ToString();
+
+            //foreach (var dir in _input.StepBy(2))
+            //{
+            //    currentPos += dir.ToSize();
+            //    positions.Add(currentPos);
+            //}
+            //currentPos = new Point(0, 0);
+            //foreach (var dir in _input.StepBy(2, 1))
+            //{
+            //    currentPos += dir.ToSize();
+            //    positions.Add(currentPos);
+            //}
+            //return new HashSet<Point>(positions).Count.ToString();
+        }
+    }
+}
