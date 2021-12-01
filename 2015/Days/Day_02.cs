1215ed26a806dfc4c7d723225b96a10e1dfb0a58
Julius Heins <Julius.JH.Heins@bmw.de>
2021-12-01 14:51:46 +0100

    Solve Day 2015 2

---
+++ 2015/Days/Day_02.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.Linq;
+
+using Core;
+using Core.Combinatorics;
+
+using static MoreLinq.Extensions.WindowExtension;
+
+namespace AoC_2015.Days
+{
+    public class Day_02 : BaseDay
+    {
+        private readonly List<int[]> _input;
+
+        public Day_02()
+        {
+            _input = File.ReadAllLines(InputFilePath).Select(line => line.ParseNNInts(3)).ToList();
+        }
+
+        private IEnumerable<int> FaceAreas(int[] s)
+        {
+            yield return s[0] * s[1];
+            yield return s[1] * s[2];
+            yield return s[2] * s[0];
+        }
+
+        private IEnumerable<int> FacePerimeters(int[] s)
+        {
+            yield return 2 * (s[0] + s[1]);
+            yield return 2 * (s[1] + s[2]);
+            yield return 2 * (s[2] + s[0]);
+        }
+
+        private int CalculateArea(int[] s) => 2 * FaceAreas(s).Sum();
+        private int SmallestFace(int[] s) => FaceAreas(s).Min();
+
+        private long Volume(int[] s) => s.Product();
+
+        public override async ValueTask<string> Solve_1()
+        {
+            return _input.Select(gift => CalculateArea(gift) + SmallestFace(gift)).Sum().ToString();
+        }
+
+        public override async ValueTask<string> Solve_2()
+        {
+            return _input.Select(gift => FacePerimeters(gift).Min() + Volume(gift)).Sum().ToString();
+        }
+    }
+}
---
+++ 2015/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using AoCHelper;
+
+namespace AoC_2015
+{
+    class Program
+    {
+        static void Main()
+        {
+            Solver.SolveLast();
+            _ = Console.ReadLine();
+        }
+    }
+}
