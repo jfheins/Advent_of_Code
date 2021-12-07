b6691f4247b5c11c751585247ee9af06fc403efe
Julius F. Heins <jfheins@gmail.com>
2021-12-07 08:11:34 +0100

    Beautify

--- 2021/Days/Day_07.cs
+++ 2021/Days/Day_07.cs
@@ -1,52 +1,46 @@
 ﻿using Core;
-using Core.Combinatorics;
-using MoreLinq.Extensions;
-using System.IO;
-using System.Linq;
 
 namespace AoC_2021.Days
 {
     public class Day_07 : BaseDay
     {
-        private int[] _numbers;
+        private List<int> _numbers;
 
         public Day_07()
         {
-            var input = File.ReadAllText(InputFilePath).Split(",").ToArray();
-            _numbers =  input.Select(int.Parse).ToArray();
+            var input = File.ReadAllText(InputFilePath).Split(",");
+            _numbers =  input.Select(int.Parse).ToList();
         }
 
         public override async ValueTask<string> Solve_1()
         {
-            var max = _numbers.Max();
-            var cost = Enumerable.Range(0, max).Select(CalcCost1).Min();
-
-            return cost.ToString();
+            var s = new BinarySearchInt(Descending);
+            var minIdx = s.FindLast(0, _numbers.Max());
+            return OverallFuel1(minIdx).ToString();
         }
 
-        private int CalcCost1(int pos) => _numbers.Sum(p => Math.Abs(p - pos));
-        private long CalcCost2(int pos)
+        private bool Descending(int x)
+        {// descending if the number of points smaller than x is less than half
+            var pointsBelow = _numbers.Count(it => it < x);
+            return 2 * pointsBelow <= _numbers.Count;
+        }
+
+        private int OverallFuel1(int pos)
         {
-            var c = _numbers.Sum(p => Fuel(Math.Abs(p - pos)));
-            Console.WriteLine($"{pos} => {c}");
-            return c;
-            long Fuel(long dist) => (dist * (dist + 1)) / 2;
+            return _numbers.Sum(p => Math.Abs(p - pos));
+        }
+
+        private long OverallFuel2(int pos)
+        {
+            return _numbers.Sum(p => Fuel(Math.Abs(p - pos)));
+
+            static long Fuel(long dist) => (dist * (dist + 1)) / 2;
         }
 
         public override async ValueTask<string> Solve_2()
         {
-            var max = _numbers.Max();
-            var min = Enumerable.Range(0, max).Select(CalcCost2).Min();
-            //var min = int.MaxValue;
-            //for (int i = 0; i < max; i++)
-            //{
-            //    Console.WriteLine(i);
-            //    var cost = CalcCost2(i);
-            //    if (cost < min)
-            //        min = cost;
-            //}
-
-            return min.ToString();
+            var avg = (int)Math.Round(_numbers.Average());
+            return OverallFuel2(avg).ToString();
         }
     }
 }
