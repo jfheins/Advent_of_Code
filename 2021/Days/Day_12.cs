3ceb76185f280d1ea7f006f74ab611ea0db47686
Julius F. Heins <jfheins@gmail.com>
2021-12-12 15:43:31 +0100

    Make day 12 fast
    - Use int comparisons instead of strings
    - Only walk the small caves, the large one are irrelevant

--- 2021/Days/Day_12.cs
+++ 2021/Days/Day_12.cs
@@ -4,16 +4,29 @@ namespace AoC_2021.Days
 {
     public class Day_12 : BaseDay
     {
-        readonly Dictionary<string, List<string>> Edges = new();
+        readonly Dictionary<int, List<int>> Edges = new();
+        readonly List<string> NodeNames;
+        readonly int Start;
+        readonly int End;
 
         public Day_12()
         {
             var input = File.ReadAllLines(InputFilePath).Select(ParseLine).ToList();
+            var stringEdges = new Dictionary<string, List<string>>();
 
             foreach (var (left, right) in input)
             {
                 AddEdge(left, right);
                 AddEdge(right, left);
+            }
+            NodeNames = stringEdges.Keys.ToList();
+            Start = NodeNames.IndexOf("start");
+            End = NodeNames.IndexOf("end");
+
+            foreach (var smallCave in NodeNames.Where(IsSmallCave))
+            {
+                var srcIndex = NodeNames.IndexOf(smallCave);
+                Edges[srcIndex] = stringEdges[smallCave].SelectMany(ResolveNode).ToList();
             }
 
             (string src, string dest) ParseLine(string line) => line.Split("-").ToTuple2();
@@ -21,31 +34,38 @@ namespace AoC_2021.Days
             void AddEdge(string from, string to)
             {
                 if (to != "start")
-                    Edges.AddToList(from, to);
+                    stringEdges.AddToList(from, to);
+            }
+
+            IEnumerable<int> ResolveNode(string caveName)
+            {
+                return IsSmallCave(caveName)
+                    ? NodeNames.IndexOf(caveName).ToEnumerable()
+                    : stringEdges[caveName].Select(it => NodeNames.IndexOf(it));
             }
         }
+        static bool IsSmallCave(string s) => char.IsLower(s[0]);
 
         public override async ValueTask<string> Solve_1()
         {
-            return Explore("start", false).ToString();
+            return Explore(false).ToString();
         }
 
         public override async ValueTask<string> Solve_2()
         {
-            return Explore("start", true).ToString();
+            return Explore(true).ToString();
         }
 
-        private int Explore(string start, bool allowOneDuplicate)
+        private int Explore(bool allowOneDuplicate)
         {
-            var pathCount = 0; 
-            var seenSmallCaves = new HashSet<string>();
-
-            ExploreRecursively(start, allowOneDuplicate);
+            var pathCount = 0;
+            var visitedCaves = new HashSet<int>();
+            ExploreRecursively(Start, allowOneDuplicate);
             return pathCount;
 
-            void ExploreRecursively(string node, bool allowOneDuplicate)
+            void ExploreRecursively(int node, bool allowOneDuplicate)
             {
-                if (node == "end")
+                if (node == End)
                 {
                     pathCount++;
                     return;
@@ -53,22 +73,15 @@ namespace AoC_2021.Days
 
                 foreach (var neighbor in Edges[node])
                 {
-                    if (IsSmallCave(neighbor))
-                    {
-                        var isExcitingNewCave = seenSmallCaves.Add(neighbor);
+                    var isExcitingNewCave = visitedCaves.Add(neighbor);
 
-                        if (allowOneDuplicate || isExcitingNewCave)
-                            ExploreRecursively(neighbor, allowOneDuplicate && isExcitingNewCave);
+                    if (allowOneDuplicate || isExcitingNewCave)
+                        ExploreRecursively(neighbor, allowOneDuplicate && isExcitingNewCave);
 
-                        if (isExcitingNewCave)
-                            seenSmallCaves.Remove(neighbor);
-                    }
-                    else
-                        ExploreRecursively(neighbor, allowOneDuplicate);
+                    if (isExcitingNewCave)
+                        visitedCaves.Remove(neighbor);
                 }
             }
-
-            static bool IsSmallCave(string s) => char.IsLower(s[0]);
         }
     }
 }
