7e690fe215bf3da75b1d94f547ef867b512993f1
Julius F. Heins <jfheins@gmail.com>
2021-12-12 05:59:24 +0100

    Solve day 11

--- 2021/Days/Day_11.cs
+++ 2021/Days/Day_11.cs
@@ -50,45 +50,40 @@ namespace AoC_2021.Days
 
         public override async ValueTask<string> Solve_2()
         {
-            var itercount = 0;
-            checked { 
-                var flashCout = 0;
-                for (int i = 0; i < 10000; i++)
+            var flashCout = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                foreach (var item in _input)
                 {
-                    foreach (var item in _input)
-                    {
-                        _input[item.pos]++;
-                    }
-                    List<(System.Drawing.Point pos, char value)> toFlash = new();
-                    do
-                    {
-                        toFlash = _input.Where(x => x.value > '9' && x.value != '+').ToList();
-                        foreach (var item in toFlash)
-                        {
-                            _input[item.pos] = '+';
-                            flashCout++;
-                            var neigh = _input.Get8NeighborsOf(item.pos);
-                            foreach (var n in neigh.Where(x => _input[x] != '+'))
-                            {
-                                if (_input[n] >= '0' && _input[n]  <= ':')
-                                    _input[n]++;
-                            }
-                        }
-                    } while (toFlash.Any());
-
-                    if (_input.All(x => x.value == '+'))
-                    {
-                        itercount = i;
-                        break;
-                    }
-
-                    foreach (var item in _input.Where(x => x.value == '+'))
-                    {
-                        _input[item.pos] = '0';
-                    }
+                    _input[item.pos]++;
                 }
-            return itercount.ToString();
+                List<(System.Drawing.Point pos, char value)> toFlash = new();
+                do
+                {
+                    toFlash = _input.Where(x => x.value > '9' && x.value != '+').ToList();
+                    foreach (var item in toFlash)
+                    {
+                        _input[item.pos] = '+';
+                        flashCout++;
+                        var neigh = _input.Get8NeighborsOf(item.pos);
+                        foreach (var n in neigh.Where(x => _input[x] != '+'))
+                        {
+                            checked { _input[n]++; };
+                        }
+                    }
+                } while (toFlash.Any());
+
+                if (_input.All(x => x.value == '+'))
+                {
+                   return (i+101).ToString();
+                }
+
+                foreach (var item in _input.Where(x => x.value == '+'))
+                {
+                    _input[item.pos] = '0';
+                }
             }
+            return "0";
         }
     }
 }
