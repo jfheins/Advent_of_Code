019da810a60a7ba6950eb68f10e821c55acebb95
Julius F. Heins <jfheins@gmail.com>
2021-12-17 10:50:47 +0100

    Day 17 faster

--- 2021/Days/Day_17.cs
+++ 2021/Days/Day_17.cs
@@ -9,7 +9,9 @@ namespace AoC_2021.Days
     public class Day_17 : BaseDay
     {
         private Rectangle _area;
-        private List<(Size v, (int maxHeight, bool isHit) res)> _results;
+        private int _rightmostTarget;
+        private int _bottommostTarget;
+        private List<Velocity> _hits;
 
         public Day_17()
         {
@@ -17,44 +19,86 @@ namespace AoC_2021.Days
             var width = input[1] + 1 - input[0];
             var height = input[3] + 1 - input[2];
             _area = new Rectangle(input[0], input[2], width, height);
+            _rightmostTarget = input[1];
+            _bottommostTarget = input[2];
         }
 
         public override async ValueTask<string> Solve_1()
         {
-            var test = new List<Size>();
-            for (int vx = 0; vx < 1000; vx++)
+            _hits = new List<Velocity>();
+            for (int vx = 0; vx <= _rightmostTarget; vx++)
             {
-                for (int vy = -100; vy < 1000; vy++)
+                for (int vy = _bottommostTarget; vy < 1000; vy++)
                 {
-                    test.Add(new Size(vx, vy));
+                    var v = new Velocity(vx, vy);
+                    if (HitsTargetArea(v))
+                        _hits.Add(v);
                 }
             }
-            _results = test.Select(x => (v: x, res: CalculateProbe(x))).ToList();
-            var r = _results.Where(x => x.res.isHit)
-                .MaxBy(x => x.res.maxHeight);
-            return r.ToString();
-        }
-
-        private (int maxHeight, bool isHit) CalculateProbe(Size velocity)
-        {
-            var hasHit = false;
-            var maxH = 0;
-            var pos = new Point(0, 0);
-            for (int i = 0; i < 1000; i++)
-            {
-                pos += velocity;
-                var newVx = Math.Max(velocity.Width - 1, 0);
-                velocity = new Size(newVx, velocity.Height - 1);
-                if (_area.Contains(pos))
-                    hasHit = true;
-                maxH = Math.Max(maxH, pos.Y);
-            }
-            return (maxH, hasHit);
+            return _hits.Max(CalculateMaxHeight).ToString();
         }
 
         public override async ValueTask<string> Solve_2()
         {
-            return _results.Where(x => x.res.isHit).Distinct().Count().ToString();
+            return _hits.Count().ToString();
         }
+
+        private int CalculateMaxHeight(Velocity v)
+        {
+            // s = v*t + 0.5*a*t*(t-1)
+            //var a = -1;
+            if (v.Y <= 0)
+                return 0;
+            else
+                return (v.Y * (v.Y + 1)) / 2;
+        }
+
+        private bool HitsTargetArea(Velocity v)
+        {
+            var minT = (int)Math.Floor(CalcTime(_area.Bottom, v.Y));
+            var maxT = (int)Math.Ceiling(CalcTime(_area.Top, v.Y));
+
+            var xState = (pos: 0, speed: v.X);
+            for (int i = 0; i < minT; i++)
+                xState = NextX(xState);
+
+            for (int t = minT; t <= maxT; t++)
+            {
+                var posY = v.Y * t - (t * (t-1)) /2;
+                if (_area.Contains(xState.pos, posY))
+                    return true;
+                xState = NextX(xState);
+            }
+            return false;
+
+            static double CalcTime(int displacement, int speed)
+            {
+                var s = displacement;
+                var v = speed;
+                var a = -1.0;
+
+                var tmp = Math.Sqrt(a * a - 4 * a * v + 8 * a * s + 4 * v * v);
+                var t = (-tmp + a - 2 * v) / (2 * a);
+                return t;
+            }
+
+            (int pos, int speed) NextX((int pos, int speed) x)
+                => (x.pos + x.speed, x.speed > 0 ? x.speed - 1 : 0);
+        }
+    }
+
+    public readonly record struct Velocity(int X, int Y)
+    {
+        public static Point operator +(Point p, Velocity v)
+            => new(p.X + v.X, p.Y + v.Y);
+
+        public static Point operator -(Point p, Velocity v)
+            => new(p.X - v.X, p.Y - v.Y);
+
+        public static Velocity operator -(Velocity v)
+            => new(-v.X, -v.Y);
+
+        public static Velocity operator *(Velocity v, int factor)
+            => new(v.X * factor, v.Y * factor);
     }
 }
