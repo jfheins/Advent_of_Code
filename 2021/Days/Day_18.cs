1634306817f314a24a0f1a2ab41efb902de61c29
Julius F. Heins <jfheins@gmail.com>
2021-12-18 07:59:19 +0100

    Solve day 18 part 1

---
+++ 2021/Days/Day_18.cs
@@ -0,0 +1,188 @@
+﻿using Core;
+using Core.Combinatorics;
+using MoreLinq.Extensions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+namespace AoC_2021.Days
+{
+    public class Day_18 : BaseDay
+    {
+        private List<JArray> _input;
+
+        public Day_18()
+        {
+            _input = File.ReadAllLines(InputFilePath).Select(ParseLine).ToList();
+        }
+
+        private JArray ParseLine(string line)
+        {
+            return JsonConvert.DeserializeObject<JArray>(line)!;
+        }
+
+        public override async ValueTask<string> Solve_1()
+        {
+            var result = _input[0];
+            foreach (var line in _input.Skip(1))
+            {
+                result = Add(result, line);
+            }
+            var magnitude = CalcMagnitude(result);
+            return magnitude.ToString();
+        }
+
+        private int CalcMagnitude(JArray x)
+        {
+            int left;
+            if (x[0] is JArray leftTree)
+                left = CalcMagnitude(leftTree);
+            else
+                left = x[0].Value<int>();
+
+            int right;
+            if (x[1] is JArray rightTree)
+                right = CalcMagnitude(rightTree);
+            else
+                right = x[1].Value<int>();
+            return 3 * left + 2 * right;
+        }
+
+        private JArray Add(JArray a, JArray b)
+        {
+            return Reduce(new JArray { a, b });
+        }
+
+        private JArray Reduce(JArray a)
+        {
+            var didExplode = true;
+            var didSplit = true;
+
+            while (didExplode || didSplit)
+            {
+                didExplode = Explode(a);
+                if (didExplode)
+                    continue;
+                didSplit = Split(a);
+            }
+            return a;
+        }
+
+        private bool Explode(JArray a)
+        {
+            var (didExplode, _, _) = Explode(a, 4);
+            return didExplode;
+        }
+        private (bool didExplode, int? forLeft, int? forRight) Explode(JArray a, int level)
+        {
+            if (level == 0)
+            {
+                Debug.Assert(a[0] is JValue);
+                Debug.Assert(a[1] is JValue);
+                return (true, (int)a[0], (int)a[1]);
+            }
+            else
+            {
+                var leftSubtree = a[0] as JArray;
+                var rightSubtree = a[1] as JArray;
+                if (leftSubtree != null)
+                {
+                    var subresult = Explode(leftSubtree, level - 1);
+                    if (subresult.forRight is int addRight) // left exploded
+                    {
+                        if (subresult.forLeft != null)
+                            a[0] = new JValue(0);
+                        if (a[1] is JValue x)
+                            a[1] = x.Value<int>() + addRight;
+                        else if (rightSubtree != null)
+                            SubAddLeft(rightSubtree, addRight); // Add it to the leftmost value in the right subtree
+                        return (subresult.didExplode, subresult.forLeft, null);
+                    }
+                    if (subresult.didExplode)
+                        return subresult;
+                }
+                if (rightSubtree != null)
+                {
+                    var subresult = Explode(rightSubtree, level - 1);
+                    if (subresult.forLeft is int addLeft) // right exploded
+                    {
+                        if (subresult.forRight != null)
+                            a[1] = new JValue(0);
+                        if (a[0] is JValue x)
+                            a[0] = x.Value<int>() + addLeft;
+                        else if (leftSubtree != null)
+                            SubAddRight(leftSubtree, addLeft); // Add it to the rightmost value of left subtree
+                        return (subresult.didExplode, null, subresult.forRight);
+                    }
+                    if (subresult.didExplode)
+                        return subresult;
+                }
+            }
+            return (false, null, null);
+        }
+
+        private static void SubAddLeft(JArray number, int value)
+        {
+            while (number[0] is JArray left)
+                number = left;
+            number[0] = (int)number[0] + value;
+        }
+
+        private static void SubAddRight(JArray number, int value)
+        {
+            while (number[1] is JArray right)
+                number = right;
+            number[1] = (int)number[1] + value;
+        }
+
+        private bool Split(JArray x)
+        {
+            var leftSubtree = x[0] as JArray;
+            var rightSubtree = x[1] as JArray;
+            if (leftSubtree != null)
+            {
+                var didSplit = Split(leftSubtree);
+                if (didSplit) return true;
+            }
+            else
+            {
+                var left = x[0].Value<int>();
+                if (left >= 10)
+                {
+                    x[0] = SplitNumber(left);
+                    return true;
+                }
+            }
+            if (rightSubtree != null)
+            {
+                var didSplit = Split(rightSubtree);
+                if (didSplit) return true;
+            }
+            else
+            {
+                var right = x[1].Value<int>();
+                if (right >= 10)
+                {
+                    x[1] = SplitNumber(right);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private JToken SplitNumber(int number)
+        {
+            var left = number / 2;
+            var right = number - left;
+            return new JArray(left, right);
+        }
+
+        public override async ValueTask<string> Solve_2()
+        {
+            return "";
+        }
+    }
+}
