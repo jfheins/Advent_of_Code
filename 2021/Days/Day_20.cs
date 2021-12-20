9be3982cdd7a482b64a81ca9207055e568c5ee40
Julius F. Heins <jfheins@gmail.com>
2021-12-20 09:33:21 +0100

    Solve day 20

---
+++ 2021/Days/Day_20.cs
@@ -0,0 +1,79 @@
+﻿using Core;
+using Core.Combinatorics;
+using MoreLinq.Extensions;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+namespace AoC_2021.Days
+{
+    public class Day_20 : BaseDay
+    {
+        private string _algo;
+        private FiniteGrid2D<char> _img;
+
+        public Day_20()
+        {
+            var input = File.ReadAllLines(InputFilePath).ToArray();
+            _algo = input[0];
+            _img = Grid2D.FromArray(input, 2.., ..);
+        }
+
+        public override async ValueTask<string> Solve_1()
+        {
+            var grid = _img;
+            var infChar = '.';
+            var infCharToggle = _algo[0] == '#';
+            for (int i = 0; i < 2; i++)
+            {
+                grid = new FiniteGrid2D<char>(grid, 1, infChar);
+                var nextgrid = new FiniteGrid2D<char>(grid);
+                foreach (var (pos, _) in grid)
+                {
+                    var area = AreaToIndex(grid.GetPointWith8Neighbors(pos, infChar));
+                    nextgrid[pos] = _algo[area];
+                }
+                grid = nextgrid;
+                if (infCharToggle)
+                    infChar = infChar == '.' ? '#' : '.';
+            }
+            //Console.WriteLine(grid.ToString());
+            
+            return grid.Count(cell => cell.value == '#').ToString();
+        }
+
+        private int AreaToIndex(IEnumerable<(Point pos, char value)> pixels)
+        {
+            var idx = 0;
+            foreach (var item in pixels)
+            {
+                var chr = item.value;
+                idx = idx << 1 | (chr == '#' ? 1 : 0);
+            }
+            return idx;
+        }
+
+        public override async ValueTask<string> Solve_2()
+        {
+            var grid = _img;
+            var infChar = '.';
+            var infCharToggle = _algo[0] == '#';
+            for (int i = 0; i < 50; i++)
+            {
+                grid = new FiniteGrid2D<char>(grid, 1, infChar);
+                var nextgrid = new FiniteGrid2D<char>(grid);
+                foreach (var (pos, _) in grid)
+                {
+                    var area = AreaToIndex(grid.GetPointWith8Neighbors(pos, infChar));
+                    nextgrid[pos] = _algo[area];
+                }
+                grid = nextgrid;
+                if (infCharToggle)
+                    infChar = infChar == '.' ? '#' : '.';
+            }
+            //Console.WriteLine(grid.ToString());
+
+            return grid.Count(cell => cell.value == '#').ToString();
+        }
+    }
+}
