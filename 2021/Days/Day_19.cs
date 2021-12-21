283b491751bcf3fde89294d5048a33db42aa4149
Julius F. Heins <jfheins@gmail.com>
2021-12-21 16:53:00 +0100

    Make day 19 faster

--- 2021/Days/Day_19.cs
+++ 2021/Days/Day_19.cs
@@ -28,26 +28,27 @@ namespace AoC_2021.Days
             public Point3 A { get; init; }
             public Point3 B { get; init; }
             public Point3 C { get; init; }
-            public (int, int, int) Hash { get; }
+            public long Hash { get; }
 
-            public IEnumerable<Point3> Points => new[] { A, B, C };
+            public Point3[] Points { get; }
             public Point3 Sum => Points.Aggregate(Point3.Empty, (a, b) => a+b);
 
-            public (Point3 a, Point3 b) GetVectors()
+            public (Point3 a, Point3 b) CalcVectors()
             {
-                var minP = Points.OrderBy(p => p.X).ThenBy(p => p.Y).ThenBy(p => p.Z).ToArray();
-                var sideA = minP[1] - minP[0];
-                var sideB = minP[2] - minP[0];
+                var sideA = Points[1] - Points[0];
+                var sideB = Points[2] - Points[0];
                 return (sideA, sideB);
             }
 
             public Triangle(Point3 a, Point3 b, Point3 c)
             {
                 A = a; B = b; C = c;
-                Hash = new[] { a, b, c, a }.PairwiseWithOverlap()
-                .Select(t => t.Item1.ManhattanDistTo(t.Item2))
-                .OrderBy(x => x)
-                .ToTuple3();
+                var distances = new int[] { a.ManhattanDistTo(b), b.ManhattanDistTo(c), c.ManhattanDistTo(a) };
+                Array.Sort(distances);
+                Hash = ((long)distances[2]) << 40 | ((long)distances[1] << 20) | ((long)distances[0]);
+                Debug.Assert(distances.Max() < (1 << 20));
+
+                Points = new[] { A, B, C }.OrderBy(p => p.X).ThenBy(p => p.Y).ThenBy(p => p.Z).ToArray();
             }
         }
 
@@ -78,26 +79,16 @@ namespace AoC_2021.Days
 
                 var intersectingTriangles = Overlap(known, next);
 
-                var knownTri = known.Triangles
-                    .Where(t => intersectingTriangles.Contains(t.Hash)).ToList();
-                var p = knownTri.SelectMany(t => t.Points).ToHashSet();
-
+                var knownTri = known.Triangles.Where(t => intersectingTriangles.Contains(t.Hash)).ToList();
                 var nextTri = knownTri.Select(kt => next.Triangles.First(t => t.Hash == kt.Hash)).ToList();
 
                 // Rotation
-                var rot = Matrix4x4.Identity;
-                var rotatedTriangles = next.Triangles;
-                foreach (var potRot in Rotations())
+                var rotated = Rotations().Select(potRot =>
                 {
-                    rotatedTriangles = Rotate(next.Triangles, potRot);
-                    var matches = rotatedTriangles.Sum(rot => knownTri.Count(k => IsKongruent(rot, k)));
-                    if (matches > 160)
-                    {
-                        Console.WriteLine(matches);
-                        rot = potRot;
-                        break;
-                    }
-                }
+                    var rotatedTriangles = Rotate(nextTri, potRot);
+                    var matches = rotatedTriangles.Zip(knownTri, (rotated, known) => IsKongruent(rotated, known)).Count(x => x);
+                    return (isMatch: matches > 200, rotation: potRot, triangles: rotatedTriangles);
+                }).First(t => t.isMatch);
 
                 // Find a triangle in both collections that is actually congruent
                 // Read: not only same side lengths
@@ -106,15 +97,15 @@ namespace AoC_2021.Days
                 while (match == null)
                 {
                     knownIdx++;
-                    match = rotatedTriangles.FirstOrDefault(t => IsKongruent(t, knownTri[knownIdx]));
+                    match = rotated.triangles.FirstOrDefault(t => IsKongruent(t, knownTri[knownIdx]));
                 }
 
                 var offset3 = match.Sum - knownTri[knownIdx].Sum;
                 var offset = new Point3(offset3.X / 3, offset3.Y / 3, offset3.Z / 3);
 
                 var newScannerPos = offset.Inverse();
-
-                var offsetScanner = Scanner.WithOffset(next, rot, newScannerPos);
+                
+                var offsetScanner = Scanner.WithOffset(next, rotated.rotation, newScannerPos);
                 offsetScanner.Position = newScannerPos;
                 _knownScanners.Add(offsetScanner);
 
@@ -122,8 +113,6 @@ namespace AoC_2021.Days
                 {
                     beacons.Add(newBeacon);
                 }
-
-                Console.WriteLine($"Added {offsetScanner.Name} at {newScannerPos}");
             }
             return beacons.Count.ToString();
         }
@@ -151,10 +140,8 @@ namespace AoC_2021.Days
 
             Triangle RotateTriangle(Triangle t) => new(rot(t.A), rot(t.B), rot(t.C));
             Point3 rot(Point3 p)
-            { 
-                var v = new Vector3(p.X, p.Y, p.Z);
-                var rotated = Vector3.Transform(v, matrix);
-                Debug.Assert(v.LengthSquared() == rotated.LengthSquared());
+            {
+                var rotated = Vector3.Transform(p.ToVector(), matrix);
                 return new Point3((int)rotated.X, (int)rotated.Y, (int)rotated.Z);
             }
         }
@@ -183,7 +170,7 @@ namespace AoC_2021.Days
             return (known, next);
         }
 
-        private static IReadOnlyCollection<(int, int, int)> Overlap(
+        private static IReadOnlySet<long> Overlap(
             Scanner a,
             Scanner b)
         {
@@ -209,26 +196,11 @@ namespace AoC_2021.Days
                     var up = Vector3.Cross(face, right);
                     if (up.LengthSquared() > 0)
                     {
-                        // rotations matrix
-                        // p = face*px + up*py + right*pz
-
-                        if (face.X > 0.8 && right.Y > 0.8)
-                        {
-                            Console.WriteLine();
-                        }
-
                         yield return new Matrix4x4(
                             face.X, right.X, up.X, 0,
                             face.Y, right.Y, up.Y, 0,
                             face.Z, right.Z, up.Z, 0,
                             0, 0, 0, 1);
-
-                        // Likely not needed
-                        //yield return new Matrix4x4(
-                        //    face.X, right.X, -up.X, 0,
-                        //    face.Y, right.Y, -up.Y, 0,
-                        //    face.Z, right.Z, -up.Z, 0,
-                        //    0, 0, 0, 1);
                     }
                 }
             }
@@ -245,10 +217,9 @@ namespace AoC_2021.Days
             if (left.Hash != right.Hash)
                 return false;
             // Same dimensions
-            var (lefta, leftb) = left.GetVectors();
-            var (righta, rightb) = right.GetVectors();
+            var (lefta, leftb) = left.CalcVectors();
+            var (righta, rightb) = right.CalcVectors();
 
-            if (lefta.ManhattanDistTo(Point3.Empty) == righta.ManhattanDistTo(Point3.Empty)) ;
             return lefta == righta && leftb == rightb;
         }
     }
