e97f229338f78acd8a23533c13da233f78d8ca2e
Julius F. Heins <jfheins@gmail.com>
2021-12-22 16:15:50 +0100

    Beautify

--- 2021/Days/Day_22.cs
+++ 2021/Days/Day_22.cs
@@ -1,85 +1,70 @@
 ﻿using Core;
-using Core.Combinatorics;
-using MoreLinq.Extensions;
-using System.IO;
-using System.Linq;
-using System.Numerics;
 
 namespace AoC_2021.Days
 {
     public class Day_22 : BaseDay
     {
-        private string[] _input;
-        private (string cmd, int[] area)[] _steps;
+        private readonly IReadOnlyList<(Cuboid volume, string cmd)> _steps;
 
         public Day_22()
         {
-            _input = File.ReadAllLines(InputFilePath).ToArray();
-            _steps = _input.Select(line => (cmd: line[0..2], area: line.ParseInts(6))).ToArray();
+            var input = File.ReadAllLines(InputFilePath);
+            _steps = input.Select(ParseCuboid).ToList();
         }
 
         public override async ValueTask<string> Solve_1()
         {
-            var aoi = new Dictionary<Point3, bool>();
-            var area2 = new Cube(new Point3(-50, -50, -50), 101);
+            var regionOfInterest = new Cube(new Point3(-50, -50, -50), 101);
+            return CalculateOnCubes(_steps.SelectMany(IntersectWithRoI)).ToString();
 
-            foreach (var (cmd, area) in _steps)
+            IEnumerable<(Cuboid volume, string cmd)> IntersectWithRoI((Cuboid volume, string cmd) t)
             {
-                var minx = Math.Max(area[0], area2.BottomLeft.X);
-                var maxx = Math.Min(area[1], area2.TopRight.X);
-                for (int x = minx; x <= maxx; x++)
-                {
-                    var miny = Math.Max(area[2], area2.BottomLeft.Y);
-                    var maxy = Math.Min(area[3], area2.TopRight.Y);
-                    for (int y = miny; y <= maxy; y++)
-                    {
-                        var minz = Math.Max(area[4], area2.BottomLeft.Z);
-                        var maxz = Math.Min(area[5], area2.TopRight.Z);
-                        for (int z = minz; z <= maxz; z++)
-                        {
-                            var p = new Point3(x, y, z);
-                            if (cmd == "on")
-                                aoi[p] = true;
-                            else
-                                aoi[p] = false;
-                        }
-                    }
-                }
+                var intersection = t.volume.Intersect(regionOfInterest);
+                if (intersection != null)
+                    yield return (intersection, t.cmd);
             }
-            return aoi.Where(kvp => area2.Contains(kvp.Key)).Count(kvp => kvp.Value).ToString();
         }
 
         public override async ValueTask<string> Solve_2()
         {
-            var volumes = new List<(Cuboid p, int sign)>();
-            foreach (var (cmd, area) in _steps)
-            {
-                var changeArea = new Cuboid
-                {
-                    Location = new Point3(area[0], area[2], area[4]),
-                    Width = area[1] - area[0] + 1,
-                    Height = area[3] - area[2] + 1,
-                    Depth = area[5] - area[4] + 1
-                };
+            return CalculateOnCubes(_steps).ToString();
+        }
 
-                // Level the playing field and "remove" any existing on areas by adding
-                // them with opposite sign.
+        private (Cuboid volume, string cmd) ParseCuboid(string line)
+        {
+            var cmd = line[0..2];
+            var ints = line.ParseInts(6);
+            var volume = new Cuboid
+            {
+                Location = new Point3(ints[0], ints[2], ints[4]),
+                Width = ints[1] - ints[0] + 1,
+                Height = ints[3] - ints[2] + 1,
+                Depth = ints[5] - ints[4] + 1
+            };
+            return (volume, cmd);
+        }
+
+        private static long CalculateOnCubes(IEnumerable<(Cuboid volume, string cmd)> input)
+        {
+            var volumes = new Dictionary<Cuboid, int>();
+            foreach (var (cuboid, cmd) in input)
+            {
+                // Level the playing field and "remove" any existing on areas
+                // either by deleting (if the same size exists) or by adding with opposite sign.
                 // in case some areas have also been switched off later, we need to cancel that as well
-                foreach (var (p, sign) in volumes.ToList())
+                foreach (var (previous, sign) in volumes.ToList())
                 {
-                    var intersection = p.Intersect(changeArea);
+                    var intersection = previous.Intersect(cuboid);
                     if (intersection != null)
                     {
                         var cancelSign = -sign;
-                        volumes.Add((intersection, cancelSign));
+                        volumes.AddOrModify(intersection, 0, x => x + cancelSign);
                     }
                 }
                 if (cmd == "on")
-                    volumes.Add((changeArea, 1));
+                    volumes.AddOrModify(cuboid, 0, val => val + 1);
             }
-
-            var sum = volumes.Aggregate(BigInteger.Zero, (sum, x) => sum + x.p.Size * x.sign);
-            return sum.ToString();
+            return volumes.Sum(kvp => kvp.Key.Size * kvp.Value);
         }
     }
 }
