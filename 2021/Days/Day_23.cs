0041edac5151e39f14f4be3cd85acd3da9ff7fce
Julius F. Heins <jfheins@gmail.com>
2021-12-23 13:26:08 +0100

    Solve part 2

--- 2021/Days/Day_23.cs
+++ 2021/Days/Day_23.cs
@@ -11,15 +11,41 @@ namespace AoC_2021.Days
 {
     public class Day_23 : BaseDay
     {
-        public record State
+        public record State : IEquatable<State>
         {
             // 01.2.3.4.56
             public ImmutableList<char> WaitingSpaces { get; set; }
 
             public ImmutableStack<char>[] Rooms { get; set; } = new ImmutableStack<char>[4];
+
+            public override string ToString()
+            {
+                return string.Join("\r\n", new string(WaitingSpaces.ToArray()),
+                    new string(Rooms[0].ToArray()),
+                    new string(Rooms[1].ToArray()),
+                    new string(Rooms[2].ToArray()),
+                    new string(Rooms[3].ToArray()));
+            }
+            public virtual bool Equals(State? other)
+            {
+                if (other == null)
+                    return false;
+                return Enumerable.SequenceEqual(WaitingSpaces, other.WaitingSpaces)
+                    && Enumerable.SequenceEqual(Rooms[0], other.Rooms[0])
+                    && Enumerable.SequenceEqual(Rooms[1], other.Rooms[1])
+                    && Enumerable.SequenceEqual(Rooms[2], other.Rooms[2])
+                    && Enumerable.SequenceEqual(Rooms[3], other.Rooms[3]);
+            }
+
+            public override int GetHashCode()
+            {
+                return new string(WaitingSpaces.ToArray()).GetHashCode();
+            }
         }
 
         private FiniteGrid2D<char> _grid;
+
+        private int stackdepth = 2;
 
         public Day_23()
         {
@@ -28,9 +54,6 @@ namespace AoC_2021.Days
 
         public override async ValueTask<string> Solve_1()
         {
-            var s = new AStarSearch<char[]>(EqualityComparer<char[]>.Default, Expander);
-
-
             var room1 = MakeRoom1(3);
             var room2 = MakeRoom1(5);
             var room3 = MakeRoom1(7);
@@ -41,12 +64,28 @@ namespace AoC_2021.Days
                 Rooms = new ImmutableStack<char>[] { room1, room2, room3, room4 },
                 WaitingSpaces = ImmutableList.Create(".......".ToCharArray())
             };
-            var waitingSpaces = ".......";
-            var slots = string.Concat(_grid[3, 2], _grid[3, 3], _grid[5, 2], _grid[5, 3],
-                _grid[7, 2], _grid[7, 3], _grid[9, 2], _grid[9, 3]);
 
-            var initialState = (waitingSpaces + slots).ToCharArray();
-            var path = s.FindFirst(initialState, Check, Heuristic)!;
+            var s = new AStarSearch<State>(null, Expander);
+            var path = s.FindFirst(initial, Check, Heuristic)!;
+            return path.Cost.ToString();
+        }
+
+        public override async ValueTask<string> Solve_2()
+        {
+            stackdepth = 4;
+            var room1 = MakeRoom2(3);
+            var room2 = MakeRoom2(5);
+            var room3 = MakeRoom2(7);
+            var room4 = MakeRoom2(9);
+
+            var initial = new State
+            {
+                Rooms = new ImmutableStack<char>[] { room1, room2, room3, room4 },
+                WaitingSpaces = ImmutableList.Create(".......".ToCharArray())
+            };
+
+            var s = new AStarSearch<State>(null, Expander);
+            var path = s.FindFirst(initial, Check, Heuristic)!;
             return path.Cost.ToString();
         }
 
@@ -56,153 +95,154 @@ namespace AoC_2021.Days
             return ImmutableStack.Create(items);
         }
 
-        private float Heuristic(char[] arg)
+        private ImmutableStack<char> MakeRoom2(int x)
         {
-            var remainder = 0;
-            var idx = arg.Select((pod, idx) => (pod, idx)).Where(x => char.IsLetter(x.pod)).ToLookup(x => x.pod, x => x.idx);
-            foreach (var pods in idx)
+            var newLines = new string[] { "#D#C#B#A#", "#D#B#A#C#" };
+            var items = new char[] { _grid[x, 3], newLines[1][x-2], newLines[0][x-2], _grid[x, 2] };
+            return ImmutableStack.Create(items);
+        }
+
+        private bool Check(State state)
+        {
+            return state.WaitingSpaces.All(it => it == '.')
+                && state.Rooms[0].All(it => it == 'A')
+                && state.Rooms[1].All(it => it == 'B')
+                && state.Rooms[2].All(it => it == 'C')
+                && state.Rooms[3].All(it => it == 'D');
+        }
+
+        private float Heuristic(State s)
+        {
+            var res = 0;
+            for (int i = 0; i < 3; i++)
             {
-                var todo = pods.Select(idx => IdxToPoint[idx].ManhattanDistTo(HomeSlot[pods.Key]));
-                remainder += (todo.Sum() - 1) * GetStepCost(pods.Key);
+                var room = s.Rooms[i];
+                var ds = room.Select((pod, i) => (pod, i)).Where(t => t.pod == 'D');
+                var len1 = ds.Select(t => t.i + 1);
+                var len2 = (3 - i) * 2;
+                res += len1.Sum(l => l + len2) * 1000;
             }
-            return remainder;
+            return res;
         }
 
-        public override async ValueTask<string> Solve_2()
+        private Dictionary<int, int> WaitingIdxToX = new()
         {
-            return "";
-        }
-
-        private bool Check(char[] state)
-        {
-            return state[11] == 'C' && state[13] == 'D' && new string(state) == ".......AABBCCDD";
-        }
-
-        private Dictionary<int, Point> IdxToPoint = new()
-        {
-            { 0, new(1, 1) },
-            { 1, new(2, 1) },
-            { 2, new(4, 1) },
-            { 3, new(6, 1) },
-            { 4, new(8, 1) },
-            { 5, new(10, 1) },
-            { 6, new(11, 1) },
-            { 7, new(3, 2) },
-            { 8, new(3, 3) },
-            { 9, new(5, 2) },
-            { 10, new(5, 3) },
-            { 11, new(7, 2) },
-            { 12, new(7, 3) },
-            { 13, new(9, 2) },
-            { 14, new(9, 3) },
+            { 0, 1 },
+            { 1, 2 },
+            { 2, 4 },
+            { 3, 6 },
+            { 4, 8 },
+            { 5, 10 },
+            { 6, 11 },
         };
 
-        private Dictionary<char, Point> HomeSlot = new()
+        private Dictionary<int, int> RoomIdxToX = new()
         {
-            { 'A', new(3, 2) },
-            { 'B', new(5, 2) },
-            { 'C', new(7, 2) },
-            { 'D', new(9, 2) },
+            { 0, 3 },
+            { 1, 5 },
+            { 2, 7 },
+            { 3, 9 },
         };
 
-        private IEnumerable<(char[], float)> Expander(char[] node)
+        private IEnumerable<(State node, float edgeCost)> Expander(State state)
         {
-            // Possible moves:
-            // any pod can move out into a available waiting spot
-            // any pod in a waiting spot can move into the right room
-
-            /*
-             ################
-             #01.2..3..4..56#
-             ###7# 9#11#13###
-               #8#10#12#14#
-               ############
-             */
-            var stateStr1 = string.Concat(node[..7]);
-            var stateStr2 = string.Concat(node[7..]);
             var param = new[] {
-                (slot: 0, idx: 7, name: 'A'),
-                (slot: 1, idx: 9, name: 'B'),
-                (slot: 2, idx: 11, name: 'C'),
-                (slot: 3, idx: 13, name: 'D') };
-
+                (room: 0, name: 'A'),
+                (room: 1, name: 'B'),
+                (room: 2, name: 'C'),
+                (room: 3, name: 'D') };
+            
             // Move out
-            foreach (var (slot, idx, name) in param)
+            foreach (var (roomIdx, name) in param)
             {
-                if (node[idx] != name || node[idx + 1] != name)
+                var room = state.Rooms[roomIdx];
+                var isDone = room.All(it => it == name);
+
+                if (isDone || room.IsEmpty)
+                    continue;
+
+                foreach (var space in FreeWaitingSpaces(roomIdx, state.WaitingSpaces))
                 {
-                    if (char.IsLetter(node[idx]))
-                    {
-                        foreach (var space in FreeWaitingSpaces(slot, node))
-                            yield return MovePod(idx, space);
-                    }
-                    else if (char.IsLetter(node[idx + 1]) && node[idx + 1] != name)
-                    {
-                        // Partially empty
-                        foreach (var space in FreeWaitingSpaces(slot, node))
-                            yield return MovePod(idx + 1, space);
-                    }
+                    yield return MovePodOut(roomIdx, space);
                 }
             }
-
-
             // Move in
-            foreach (var (slot, idx, name) in param)
+            foreach (var (roomIdx, name) in param)
             {
-                var waiting = OccupiedWaitingSpaces(slot, node).Where(i => node[i] == name).ToList();
-                foreach (var pod in waiting)
+                var room = state.Rooms[roomIdx];
+                var isDone = room.All(it => it == name);
+
+                if (room.Count() < stackdepth && isDone)
                 {
-                    if (node[idx] == '.' && node[idx + 1] == '.')
-                        yield return MovePod(pod, idx + 1);
-                    if (node[idx] == '.' && node[idx + 1] == name)
-                        yield return MovePod(pod, idx);
+                    var waiting = OccupiedWaitingSpaces(roomIdx, state).Where(i => state.WaitingSpaces[i] == name).ToList();
+                    foreach (var space in waiting)
+                        yield return MovePodIn(roomIdx, space);
                 }
             }
 
-            (char[], float) MovePod(int from, int to)
+            (State, float) MovePodOut(int fromRoom, int toIdx)
             {
-                Debug.Assert(char.IsLetter(node[from]));
-                Debug.Assert(node[to] == '.');
-                var newState = (char[])node.Clone();
-                newState[to] = node[from];
-                newState[from] = '.';
-                var stepCost = GetStepCost(node[from]);
-                var cost = IdxToPoint[from].ManhattanDistTo(IdxToPoint[to]) * stepCost;
-                return (newState, cost);
+                var newRooms = (ImmutableStack<char>[])state.Rooms.Clone();
+                var mover = state.Rooms[fromRoom].Peek();
+                newRooms[fromRoom] = state.Rooms[fromRoom].Pop();
+                var newHallway = state.WaitingSpaces.SetItem(toIdx, mover);
+
+                var len1 = stackdepth - newRooms[fromRoom].Count(); // 1 or 2
+                var len2 = Math.Abs(WaitingIdxToX[toIdx] - RoomIdxToX[fromRoom]);
+                var stepCost = GetStepCost(mover);
+
+                var cost = stepCost * (len1 + len2);
+                return (new State { Rooms = newRooms, WaitingSpaces = newHallway }, cost);
+            }
+
+            (State, float) MovePodIn(int toRoom, int fromSpace)
+            {
+                var newRooms = (ImmutableStack<char>[])state.Rooms.Clone();
+                var mover = state.WaitingSpaces[fromSpace];
+                newRooms[toRoom] = state.Rooms[toRoom].Push(mover);
+                var newHallway = state.WaitingSpaces.SetItem(fromSpace, '.');
+
+                var len1 = stackdepth - state.Rooms[toRoom].Count(); // 1 or 2
+                var len2 = Math.Abs(WaitingIdxToX[fromSpace] - RoomIdxToX[toRoom]);
+                var stepCost = GetStepCost(mover);
+
+                var cost = stepCost * (len1 + len2);
+                return (new State { Rooms = newRooms, WaitingSpaces = newHallway }, cost);
             }
         }
 
         private static int GetStepCost(char podName)
             => podName switch { 'A' => 1, 'B' => 10, 'C' => 100, 'D' => 1000, _ => throw new NotImplementedException() };
 
-        private IEnumerable<int> FreeWaitingSpaces(int slotIdx, char[] state)
+
+        private IEnumerable<int> FreeWaitingSpaces(int roomIdx, ImmutableList<char> hallway)
         {
-            var first = slotIdx + 1;
+            var first = roomIdx + 1;
             // left
             for (int i = first; i >= 0; i--)
-                if (state[i] == '.')
+                if (hallway[i] == '.')
                     yield return i;
                 else
                     break;
             for (int i = first + 1; i < 7; i++)
-                if (state[i] == '.')
+                if (hallway[i] == '.')
                     yield return i;
                 else
                     break;
         }
 
-        private IEnumerable<int> OccupiedWaitingSpaces(int slotIdx, char[] state)
+        private IEnumerable<int> OccupiedWaitingSpaces(int roomIdx, State state)
         {
-            var first = slotIdx + 1;
+            var first = roomIdx + 1;
             // left
             for (int i = first; i >= 0; i--)
-                if (state[i] != '.')
+                if (state.WaitingSpaces[i] != '.')
                 {
                     yield return i;
                     break;
                 }
             for (int i = first + 1; i < 7; i++)
-                if (state[i] != '.')
+                if (state.WaitingSpaces[i] != '.')
                 {
                     yield return i;
                     break;
