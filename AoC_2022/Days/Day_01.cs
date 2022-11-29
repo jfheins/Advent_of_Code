b6767e655177d293e8fdee762fe583f507ada18c
Julius Heins <Julius.JH.Heins@bmw.de>
2022-11-29 16:47:03 +0100

    Prepare

---
+++ AoC_2022/Days/Day_01.cs
@@ -0,0 +1,24 @@
+﻿using Core;
+
+namespace AoC_2022.Days
+{
+    public sealed class Day_01 : BaseDay
+    {
+        private readonly int[] _input;
+
+        public Day_01()
+        {
+            _input = File.ReadAllLines(InputFilePath).Select(int.Parse).ToArray();
+        }
+
+        public override async ValueTask<string> Solve_1()
+        {
+            return "";
+        }
+
+        public override async ValueTask<string> Solve_2()
+        {
+            return "";
+        }
+    }
+}
---
+++ AoC_2022/Program.cs
@@ -0,0 +1,18 @@
+﻿using AoCHelper;
+
+namespace AoC_2022
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Solver.SolveLast(new SolverConfiguration
+            {
+                ShowConstructorElapsedTime = true,
+                ShowTotalElapsedTimePerDay = true,
+                ElapsedTimeFormatSpecifier = "0.0",
+            });
+            _ = Console.ReadLine();
+        }
+    }
+}
