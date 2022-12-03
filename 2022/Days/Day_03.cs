be5c7f9d9e7274375897ca7a9da0a20b2df4371c
Julius F. Heins <jfheins@gmail.com>
2022-12-03 06:44:39 +0100

    Refactor day 3

--- 2022/Days/Day_03.cs
+++ 2022/Days/Day_03.cs
@@ -9,6 +9,7 @@ namespace AoC_2022.Days
     public sealed class Day_03 : BaseDay
     {
         private string[] _input;
+        private string prio = " " + Constants.LowerCaseAbc + Constants.UpperCaseAbc;
 
         public Day_03()
         {
@@ -17,30 +18,27 @@ namespace AoC_2022.Days
 
         public override async ValueTask<string> Solve_1()
         {
-            var prio = "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            long sum = 0;
-            foreach (var line in _input)
+            return _input.Sum(CalcPriority).ToString();
+
+            int CalcPriority(string line)
             {
                 var half = line.Length / 2;
-                var left = line.Take(half).ToHashSet();
-                var right = line.Skip(half).ToHashSet();
-                //Debug.Assert(left.Count() == right.Count());
+                var left = line[..half];
+                var right = line[half..];
                 var common = left.Intersect(right);
-                sum += prio.IndexOf(common.Single());
+                return prio.IndexOf(common.Single());
             }
-            return sum.ToString();
         }
 
         public override async ValueTask<string> Solve_2()
         {
-            var prio = "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            var sum = 0;
-            foreach (var group in _input.Chunk(3))
+            return _input.Chunk(3).Sum(CalcPriority).ToString();
+
+            int CalcPriority(string[] group)
             {
                 var common = group[0].Intersect(group[1]).Intersect(group[2]);
-                sum += prio.IndexOf(common.Single());
+                return prio.IndexOf(common.Single());
             }
-            return sum.ToString();
         }
     }
 }
---
+++ Core/Core/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Core
+{
+    public static class Constants
+    {
+        public const string LowerCaseAbc = "abcdefghijklmnopqrstuvwxyz";
+        public const string UpperCaseAbc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+}
