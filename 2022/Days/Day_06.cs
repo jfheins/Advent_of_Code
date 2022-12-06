14163ab5302a37027756dfae947af57863369db3
Julius F. Heins <jfheins@gmail.com>
2022-12-06 06:56:41 +0100

    Solve day 6

---
+++ 2022/Days/Day_06.cs
@@ -0,0 +1,41 @@
+﻿using Core;
+using System.Diagnostics;
+using System.Collections.Immutable;
+
+using static MoreLinq.Extensions.SplitExtension;
+using static MoreLinq.Extensions.TransposeExtension;
+
+namespace AoC_2022.Days
+{
+    public sealed class Day_06 : BaseDay
+    {
+        private readonly string _input;
+
+        public Day_06()
+        {
+            _input = File.ReadAllText(InputFilePath);
+        }
+
+        public override async ValueTask<string> Solve_1()
+        {
+            for (int i = 4; i < _input.Length; i++)
+            {
+                var last4 = _input.Substring(i - 4, 4);
+                if (last4.Distinct().Count() == 4)
+                    return i.ToString();
+            }
+            return "-";
+        }
+
+        public override async ValueTask<string> Solve_2()
+        {
+            for (int i = 14; i < _input.Length; i++)
+            {
+                var last4 = _input.Substring(i - 14, 14);
+                if (last4.Distinct().Count() == 14)
+                    return i.ToString();
+            }
+            return "-";
+        }
+    }
+}
