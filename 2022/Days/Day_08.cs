5690663c4290dfd382894a461adb8e2b64b1f394
Julius F. Heins <jfheins@gmail.com>
2022-12-08 06:20:30 +0100

    Solve day 8

---
+++ 2022/Days/Day_08.cs
@@ -0,0 +1,59 @@
+﻿using System.Drawing;
+using Core;
+
+using static MoreLinq.Extensions.TakeUntilExtension;
+
+namespace AoC_2022.Days
+{
+    public sealed class Day_08 : BaseDay
+    {
+        private readonly FiniteGrid2D<char> _input;
+
+        public Day_08()
+        {
+            _input = new FiniteGrid2D<char>(File.ReadAllLines(InputFilePath));
+        }
+
+        public override async ValueTask<string> Solve_1()
+        {
+            return _input.Count(t => isVisible(t.pos)).ToString();
+
+
+            return "-";
+        }
+
+        private bool isVisible(Point p)
+        {
+            var height = _input[p];
+            foreach (var dir in Directions.All4)
+            {
+                var line = _input.Line(p, dir.ToSize()).ToList();
+
+                if (line.Count == 0)
+                    return true;
+
+                if (line.All(other => _input[other] < height))
+                    return true;
+            }
+            return false;
+        }
+
+        public override async ValueTask<string> Solve_2()
+        {
+            return _input.Max(t => Score(t.pos)).ToString();
+        }
+
+        private int Score(Point p)
+        {
+            var height = _input[p];
+            var score = 1;
+            foreach (var dir in Directions.All4)
+            {
+                var line = _input.Line(p, dir.ToSize()).ToList();
+
+                score *= line.TakeUntil(other => _input[other] >= height).Count();
+            }
+            return score;
+        }
+    }
+}
