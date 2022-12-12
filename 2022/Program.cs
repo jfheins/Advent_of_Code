c813cbaec894adf1378519619bc198bfcd67293c
Julius F. Heins <jfheins@gmail.com>
2022-12-12 08:19:44 +0100

    Reset Program

--- 2022/Program.cs
+++ 2022/Program.cs
@@ -1,22 +1,12 @@
-﻿using AoC_2022.Days;
-
-using AoCHelper;
+﻿using AoCHelper;
 
 namespace AoC_2022
 {
-    static partial class Program
+    static class Program
     {
         static async Task Main()
         {
-            for (int i = 0; i < 10; i++)
-            {
-                var d = new Day_11();
-                await d.Solve_1();
-                await d.Solve_2();
-            }
-            await Task.Delay(100);
-
-            await Solver.Solve<Day_11>(c => 
+            await Solver.SolveLast(c => 
             {
                 c.ShowConstructorElapsedTime = true;
                 c.ShowTotalElapsedTimePerDay = true;
