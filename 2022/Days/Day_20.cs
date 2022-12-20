79c0c168d5d06664328dcb521923630fedb348ba
Julius F. Heins <jfheins@gmail.com>
2022-12-20 08:22:52 +0100

    Solve part 1

---
+++ 2022/Days/Day_20.cs
@@ -0,0 +1,81 @@
+﻿using Core;
+
+using Spectre.Console;
+
+using System.Data;
+using System.Runtime.Intrinsics;
+
+namespace AoC_2022.Days
+{
+    public sealed class Day_20 : BaseDay
+    {
+        private IReadOnlyList<int> _input;
+
+        public Day_20()
+        {
+            _input = File.ReadAllLines(InputFilePath).SelectList(int.Parse);
+        }
+
+        public override async ValueTask<string> Solve_1()
+        {
+            var ll = new LinkedList<int>();
+            foreach (var item in _input)
+            {
+               _= ll.AddLast(item);
+            }
+            var end = ll.Last;
+            var current = ll.First;
+            var wasMoved = new HashSet<LinkedListNode<int>>();
+
+            while(current != null)
+            {
+                if (!wasMoved.Add(current))
+                {
+                    current = current.Next;
+                    continue;
+                }
+                var next = current.Next;
+                var toMove = current.Value;
+                while(toMove < 0)
+                {
+                    var pred = current.Previous ?? ll.Last!;
+                    ll.Remove(current);
+                    if (pred == ll.First)
+                    {
+                        ll.AddLast(current);
+                    }
+                    else
+                    {
+                        ll.AddBefore(pred, current);
+                    }
+                    toMove++;
+                }
+                while (toMove > 0)
+                {
+                    var succ = current.Next ?? ll.First!;
+                    ll.Remove(current);
+                    ll.AddAfter(succ, current);
+                    toMove--;
+                }
+                current = next;
+            }
+            var zeroIdx = ll.IndexWhere(it => it == 0).First();
+
+            return (GetValue(zeroIdx+1000, ll) + GetValue(zeroIdx+2000, ll) + GetValue(zeroIdx+3000, ll)).ToString();
+        }
+
+        public T GetValue<T>(int idx, LinkedList<T> list)
+        {
+            var rem = idx.Modulo(list.Count);
+            var node = list.First;
+            while (rem-- > 0)
+                node = node.Next ?? list.First!;
+            return node.Value;
+        }
+
+        public override async ValueTask<string> Solve_2()
+        {
+            return "-";
+        }
+    }
+}
