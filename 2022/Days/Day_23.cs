29dc109a1ddaab11202e334ce5b4c6ddfd89422b
Julius F. Heins <jfheins@gmail.com>
2022-12-23 07:18:51 +0100

    Beautify

--- 2022/Days/Day_23.cs
+++ 2022/Days/Day_23.cs
@@ -6,109 +6,73 @@ namespace AoC_2022.Days
 {
     public sealed class Day_23 : BaseDay
     {
-        private FiniteGrid2D<char> _map;
+        private readonly FiniteGrid2D<char> _map;
+        private readonly List<Direction> _directions;
         private readonly string[] _input;
 
         public Day_23()
         {
             _input = File.ReadAllLines(InputFilePath);
             _map = new FiniteGrid2D<char>(_input);
+            _map.RemoveWhere(p => !HasElf(p));
+            _directions = new List<Direction>() { Direction.Up, Direction.Down, Direction.Left, Direction.Right };
         }
 
         public override async ValueTask<string> Solve_1()
         {
-            _map.RemoveWhere(p => !HasElf(p));
-            var directions = new List<Direction>() { Direction.Up, Direction.Down, Direction.Left, Direction.Right };
             for (int i = 0; i < 10; i++)
-            {
-                var proposed = new List<(Point from, Point to)>();
-                foreach (var elf in _map.Where(t => t.value == '#'))
-                {
-                    var n = _map.Get8NeighborsOf(elf.pos);
-                    if(n.Any(HasElf))
-                    {
-                        foreach (var dir in directions)
-                        {
-                            var one = elf.pos.MoveTo(dir);
-                            var two = one.MoveTo(dir.TurnClockwise());
-                            var three = one.MoveTo(dir.TurnCounterClockwise());
+                _ = PlayRound();
 
-                            if (!HasElf(one) && !HasElf(two) && !HasElf(three))
-                            {
-                                proposed.Add((elf.pos, one));
-                                break;
-                            }
-                        }
-                    }
-                }
-                foreach (var (from, to) in proposed.ToLookup(it => it.to).Where(it => it.Count() == 1).Select(it => it.First()))
-                {
-                    _map.RemoveAt(from);
-                    _map[to] = '#';
-                }
-                directions.Add(directions.First());
-                directions.RemoveAt(0);
-                _map.SizeToFit();
-                proposed.Clear();
-            }
-            var c1 = _map.Count;
             _map.SizeToFit();
             _map.FillGaps('.');
-            var c = _map.Count;
 
-            return _map
-                .Count(t => t.value == '.')
+            return _map.Count(t => t.value == '.')
                 .ToString();
-
-            bool HasElf(Point p) => _map.GetValueOrDefault(p, '.') == '#';
         }
 
         public override async ValueTask<string> Solve_2()
         {
-            _map = new FiniteGrid2D<char>(_input);
-            _map.RemoveWhere(p => !HasElf(p));
-            var directions = new List<Direction>() { Direction.Up, Direction.Down, Direction.Left, Direction.Right };
-            var rounds = 1;
-            while(true)
+            var rounds = 11;
+            while (PlayRound())
             {
-                var proposed = new List<(Point from, Point to)>();
-                foreach (var elf in _map.Where(t => t.value == '#'))
-                {
-                    var n = _map.Get8NeighborsOf(elf.pos);
-                    if (n.Any(HasElf))
-                    {
-                        foreach (var dir in directions)
-                        {
-                            var one = elf.pos.MoveTo(dir);
-                            var two = one.MoveTo(dir.TurnClockwise());
-                            var three = one.MoveTo(dir.TurnCounterClockwise());
+                rounds++;
+            }
+            return rounds.ToString();
+        }
 
-                            if (!HasElf(one) && !HasElf(two) && !HasElf(three))
-                            {
-                                proposed.Add((elf.pos, one));
-                                break;
-                            }
+        private bool PlayRound()
+        {
+            var proposed = new List<(Point from, Point to)>();
+            foreach (var (elfPos, _) in _map.Where(t => t.value == '#'))
+            {
+                if (_map.Get8NeighborsOf(elfPos).Any(HasElf))
+                {
+                    foreach (var dir in _directions)
+                    {
+                        var one = elfPos.MoveTo(dir);
+                        var two = one.MoveTo(dir.TurnClockwise());
+                        var three = one.MoveTo(dir.TurnCounterClockwise());
+
+                        if (!HasElf(one) && !HasElf(two) && !HasElf(three))
+                        {
+                            proposed.Add((elfPos, one));
+                            break;
                         }
                     }
                 }
-                foreach (var (from, to) in proposed.ToLookup(it => it.to).Where(it => it.Count() == 1).Select(it => it.First()))
-                {
-                    _map.RemoveAt(from);
-                    _map[to] = '#';
-                }
-                directions.Add(directions.First());
-                directions.RemoveAt(0);
-                _map.SizeToFit();
-                if (proposed.Any())
-                    proposed.Clear();
-                else
-                    break;
-                rounds++;
             }
-           
-            return rounds.ToString();
+            var validMoves = proposed.ToLookup(it => it.to).Where(it => it.Count() == 1).Select(it => it.First());
+            foreach (var (from, to) in validMoves)
+            {
+                _map.RemoveAt(from);
+                _map[to] = '#';
+            }
+            _directions.Add(_directions.First());
+            _directions.RemoveAt(0);
+            _map.SizeToFit();
 
-            bool HasElf(Point p) => _map.GetValueOrDefault(p, '.') == '#';
+            return proposed.Any();
         }
+        bool HasElf(Point p) => _map.GetValueOrDefault(p, '.') == '#';
     }
 }
