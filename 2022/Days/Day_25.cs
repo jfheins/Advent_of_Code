b25656a9b95e59b90073fe0655231c6bf2f74087
Julius F. Heins <jfheins@gmail.com>
2022-12-26 10:16:20 +0100

    Beautify

--- 2022/Days/Day_25.cs
+++ 2022/Days/Day_25.cs
@@ -1,7 +1,4 @@
-﻿using Core;
-
-using System.Collections.Concurrent;
-using System.Drawing;
+﻿using static MoreLinq.Extensions.ZipLongestExtension;
 
 namespace AoC_2022.Days
 {
@@ -16,31 +13,63 @@ namespace AoC_2022.Days
 
         public override async ValueTask<string> Solve_1()
         {
-            return _input.SelectList(ConvertLine).Sum().ToString(); // Use excel to convert
+            return _input.Aggregate(SumQuinary);
         }
 
-        private long ConvertLine(string line)
+        private static string SumQuinary(string num1, string num2)
         {
-            // the digits are 2, 1, 0, minus (written -), and double-minus (written =). Minus is worth -1, and double-minus is worth -2."
-            var digitWorth = 1L;
-            var result = 0L;
-            foreach (var digit in line.Reverse())
-            {
-                result += digitWorth * ConvertDigit(digit);
-                digitWorth *= 5;
-            }
-            return result;
+            var sequence = num1.Reverse().ZipLongest(num2.Reverse(), (a, b) => (a, b));
 
-            static int ConvertDigit(char digit) => digit switch
+            var result = new List<char>();
+            var carry = 0;
+            foreach (var (a, b) in sequence)
             {
-                '=' => -2,
-                '-' => -1,
-                '0' => 0,
-                '1' => 1,
-                '2' => 2,
+                var res = SumOfDigits(a, b, carry);
+                result.Add(ConvertDigit(res.sum));
+                carry = res.carry;
+            }
+            result.Add(ConvertDigit(carry));
+            result.Reverse();
+            return string.Concat(result.SkipWhile(c => c == '0'));
+        }
+
+        static (int sum, int carry) SumOfDigits(char a, char b, int carry = 0)
+            => (ConvertDigit(a) + ConvertDigit(b) + carry) switch
+            {
+                -5 => (0, -1),
+                -4 => (1, -1),
+                -3 => (2, -1),
+                -2 => (-2, 0),
+                -1 => (-1, 0),
+                0 => (0, 0),
+                1 => (1, 0),
+                2 => (2, 0),
+                3 => (-2, 1),
+                4 => (-1, 1),
+                5 => (0, 1),
                 _ => throw new NotImplementedException()
             };
-        }
+
+        static int ConvertDigit(char digit) => digit switch
+        {
+            '=' => -2,
+            '-' => -1,
+            '0' => 0,
+            '1' => 1,
+            '2' => 2,
+            '\0' => 0,
+            _ => throw new NotImplementedException()
+        };
+
+        static char ConvertDigit(int value) => value switch
+        {
+            -2 => '=',
+            -1 => '-',
+            0 => '0',
+            1 => '1',
+            2 => '2',
+            _ => throw new NotImplementedException()
+        };
 
 
         public override async ValueTask<string> Solve_2()
