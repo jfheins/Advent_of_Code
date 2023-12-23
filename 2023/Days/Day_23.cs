fff0595ce0316c901e5c4bf63a1ff5de84dfa463
Julius F. Heins <jfheins@gmail.com>
2023-12-23 09:05:41 +0100

    Solve day 23

---
+++ 2023/Days/Day_23.cs
@@ -0,0 +1,151 @@
+﻿using Core;
+
+using Microsoft.Z3;
+
+using Spectre.Console;
+
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Xml.Linq;
+
+namespace AoC_2023.Days;
+
+public sealed partial class Day_23 : BaseDay
+{
+    private readonly string[] _input;
+    private readonly FiniteGrid2D<char> _grid;
+
+    public Day_23()
+    {
+        _input = File.ReadAllLines(InputFilePath);
+        _grid = new FiniteGrid2D<char>(_input);
+    }
+
+    public override async ValueTask<string> Solve_1()
+    {
+        var start = new Point(1, 0);
+        var bottom = _grid.BottomRight.Y;
+
+        var res = new List<List<Point>>();
+        Dfs([], start, p => p.Y == bottom, res);
+
+        var longest = res.MaxBy(it => it.Count);
+
+        return (longest.Count - 1).ToString();
+    }
+
+    public override async ValueTask<string> Solve_2()
+    {
+        var start = new Point(1, 0);
+        var target = _grid.BottomRight.MoveBy(-1, 0);
+        _nodes.Add(start, []);
+        _nodes.Add(target, []);
+        foreach (var node in _nodes.Keys)
+        {
+            var bfs = new DepthFirstSearch<Point>(null, ExpandUntilNode);
+            var others = _nodes.Keys.ExceptFor(node).ToHashSet();
+            var paths = bfs.FindAll(node, others.Contains);
+            var longestPerTarget = paths.ToLookup(it => it.Target).Select(g => (g.Key, g.Max(p => p.Length)));
+
+            _nodes[node].AddRange(longestPerTarget);
+
+            IEnumerable<Point> ExpandUntilNode(Point point) 
+                => _nodes.ContainsKey(point) && point != node 
+                ? Array.Empty<Point>() 
+                : Expand2(point);
+        }
+
+        var dfs = new DepthFirstSearch<Point>(null, ExpandCoarse);
+        var res = dfs.FindAll(start, p => p == target);
+
+        return res.Max(CountSteps).ToString();
+
+        IEnumerable<Point> ExpandCoarse(Point point) => _nodes[point].Select(it => it.other);
+
+        int CountSteps(IPath<Point> p)
+        {
+            var steps = 0;
+            foreach (var (l, r) in p.Steps.PairwiseWithOverlap())
+            {
+                steps += _nodes[l].First(it => it.other == r).distance;
+            }
+            return steps;
+        }
+    }
+
+    private void Dfs(List<Point> path, Point p, Func<Point, bool> predicte, List<List<Point>> results)
+    {
+        path.Add(p);
+        if (predicte(p))
+        {
+            results.Add([.. path]);
+        }
+
+        var neighbors = Expand2(p).ToList();
+        neighbors.RemoveAll(n => _grid[n] == '#');
+        if (neighbors.Count > 2)
+        {
+            _nodes[p] = new List<(Point, int)>();
+        }
+        if ("<>^v".Contains(_grid[p]))
+        {
+            var d = _grid[p].Parse();
+            neighbors = [p.MoveTo(d)];
+        }
+        foreach (var neighbor in neighbors)
+        {
+            if (!path.Contains(neighbor))
+                Dfs(path, neighbor, predicte, results);
+        }
+        path.Remove(p);
+    }
+
+    int max = 0;
+    private Dictionary<Point, List<(Point other, int distance)>> _nodes = new();
+
+    private void Log(int c)
+    {
+        if (c > max)
+        {
+            Console.WriteLine($"Found path of l={c}");
+            max = c;
+        }
+    }
+
+    private void Dfs2(Point start, Func<Point, bool> predicate, List<List<Point>> results)
+    {
+        var work = new Stack<(Point p, HashSet<Point> path)>();
+        work.Push((start, []));
+
+        while (work.TryPop(out var current))
+        {
+            current.path.Add(current.p);
+            if (predicate(current.p))
+            {
+                results.Add(current.path.ToList());
+                Log(current.path.Count);
+                continue;
+            }
+
+            var n = Expand2(current.p).ToList();
+            n.RemoveAll(p => _grid[p] == '#' || current.path.Contains(p));
+
+            foreach (var later in n.Skip(1))
+            {
+                work.Push((later, [.. current.path]));
+            }
+            if (n.Count > 0)
+                work.Push((n[0], current.path));
+        }
+    }
+
+    private IEnumerable<Point> Expand2(Point point)
+    {
+        var tile = _grid[point];
+        if (tile != '#')
+            return _grid.Get4NeighborsOf(point);
+        else
+            return Array.Empty<Point>();
+    }
+}
---
+++ Core/Core/DepthFirstSearch.cs
@@ -0,0 +1,166 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using JetBrains.Annotations;
+
+using MoreLinq;
+
+namespace Core
+{
+    public class DepthFirstSearch<TNode> where TNode: notnull
+    {
+        public delegate void ProgressReporterCallback(int workingSetCount, int visitedCount);
+
+        private readonly NodeComparer _comparer;
+        private readonly Func<TNode, IEnumerable<TNode>> _expander;
+
+        /// <summary>
+        ///     Prepares a breadth first search.
+        /// </summary>
+        /// <param name="comparer">Comparison function that determines node equality</param>
+        /// <param name="expander">Callback to get the possible nodes from a source node</param>
+        public DepthFirstSearch(IEqualityComparer<TNode>? comparer, Func<TNode, IEnumerable<TNode>> expander)
+        {
+            _comparer = new NodeComparer(comparer ?? EqualityComparer<TNode>.Default);
+            _expander = expander;
+        }
+
+        [CanBeNull]
+        public IPath<TNode>? FindFirst(TNode initialNode,
+                                      Func<TNode, bool> targetPredicate,
+                                      ProgressReporterCallback? progressReporter = null)
+        {
+            var result = FindAll(initialNode, targetPredicate, progressReporter, 1);
+            return result.FirstOrDefault();
+        }
+
+        [NotNull]
+        public IList<IPath<TNode>> FindAll(TNode initialNode,
+                                           Func<TNode, bool> targetPredicate,
+                                           ProgressReporterCallback? progressReporter = null,
+                                           int minResults = int.MaxValue)
+        {
+            return FindAll2(initialNode, dfsnode => targetPredicate(dfsnode.Item), progressReporter, minResults);
+        }
+
+        [NotNull]
+        public IList<IPath<TNode>> FindAll2(TNode initialNode,
+                                           Func<NodeWithPredecessor, bool> targetPredicate,
+                                           ProgressReporterCallback? progressReporter = null,
+                                           int minResults = int.MaxValue)
+        {
+            if (targetPredicate == null)
+                throw new ArgumentNullException(nameof(targetPredicate), "A meaningful targetPredicate must be provided");
+
+            var results = new List<IPath<TNode>>();
+            var initial = new NodeWithPredecessor(initialNode);
+            var work = new Stack<(NodeWithPredecessor node, HashSet<TNode> past)>();
+            work.Push((initial, []));
+
+            while (work.TryPop(out var current))
+            {
+                current.past.Add(current.node.Item);
+
+                if (targetPredicate(current.node))
+                {
+                    results.Add(new DfsPath(current.node));
+                   // progressReporter?.Invoke(work.Count, results.Count);
+                }
+
+                var neighbors = _expander(current.node.Item).ToList();
+                neighbors.RemoveAll(current.past.Contains);
+
+                for (var i = neighbors.Count - 1; i >= 0; i--)
+                {
+                    var next = new NodeWithPredecessor(neighbors[i], current.node);
+                    if (i==0)
+                        work.Push((next, current.past)); // Reuse hashset
+                    else
+                        work.Push((next, [.. current.past]));
+                }                    
+            }
+
+            return results;
+        }
+
+        private IEnumerable<NodeWithPredecessor> SequentialExpand(IEnumerable<NodeWithPredecessor> nextNodes,
+                                                                  HashSet<NodeWithPredecessor> visitedNodes)
+        {
+            return nextNodes
+                .SelectMany(sourceNode => _expander(sourceNode.Item)
+                    .Select(dest => new NodeWithPredecessor(dest, sourceNode))
+                    .Where(dest => !visitedNodes.Contains(dest)));
+        }
+
+        private class DfsPath : IPath<TNode>
+        {
+            public DfsPath(TNode singleNode)
+            {
+                Target = singleNode;
+                Length = 0;
+                Steps = [singleNode];
+            }
+
+            public DfsPath(NodeWithPredecessor target)
+            {
+                Target = target.Item;
+                Steps = target.GetHistory().Reverse().ToArray();
+                Length = Steps.Length - 1;
+            }
+
+            public TNode Target { get; }
+            public int Length { get; }
+            public TNode[] Steps { get; }
+        }
+
+        private class NodeComparer : EqualityComparer<NodeWithPredecessor>
+        {
+            public readonly IEqualityComparer<TNode> _comparer;
+
+            public NodeComparer(IEqualityComparer<TNode> comparer)
+            {
+                _comparer = comparer;
+            }
+
+            public override bool Equals(NodeWithPredecessor? a, NodeWithPredecessor? b)
+            {
+                if (a is null || b is null)
+                    return ReferenceEquals(a, b);
+
+                return _comparer.Equals(a.Item, b.Item);
+            }
+
+            public override int GetHashCode(NodeWithPredecessor x)
+            {
+                return _comparer.GetHashCode(x.Item);
+            }
+        }
+
+        public class NodeWithPredecessor
+        {
+            public NodeWithPredecessor(TNode current, NodeWithPredecessor? predecessor = null)
+            {
+                Predecessor = predecessor;
+                Item = current;
+                Distance = (predecessor?.Distance + 1) ?? 0;
+            }
+
+            public TNode Item { get; }
+            public NodeWithPredecessor? Predecessor { get; }
+            public int Distance { get; set; }
+
+            public IEnumerable<TNode> GetHistory()
+            {
+                NodeWithPredecessor? pointer = this;
+                do
+                {
+                    yield return pointer.Item;
+                    pointer = pointer.Predecessor;
+                } while (pointer != null);
+            }
+        }
+    }
+}
