037233351c5c8afa31510fbcee83f49e4c6800c6
Julius F. Heins <jfheins@gmail.com>
2024-12-02 06:17:07 +0100

    Solve day 2

---
+++ 2024/Days/Day_02.cs
@@ -0,0 +1,51 @@
+﻿using Core;
+using Spectre.Console;
+using System.Drawing;
+
+namespace AoC_2024.Days;
+
+public sealed partial class Day_02 : BaseDay
+{
+    private readonly int[][] _input;
+
+    public Day_02()
+    {
+        _input = File.ReadAllLines(InputFilePath).SelectArray(x => x.ParseInts());
+    }
+
+    public override async ValueTask<string> Solve_1()
+    {
+        // var grid = new FiniteGrid2D<char>(_input);
+        var safeCount = _input.Count(IsSafe);
+        return safeCount.ToString();
+    }
+
+    bool IsSafe(int[] s)
+        => (s.Diff().All(x => x > 0)
+        || s.Diff().All(x => x < 0))
+        && s.Diff().All(x => Math.Abs(x) <= 3);
+
+    public override async ValueTask<string> Solve_2()
+    {
+        var sc = 0;
+        foreach (var s in _input)
+        {
+            if (IsSafe(s))
+                sc++;
+            else
+            {
+                for (var i = 0; i < s.Length; i++)
+                {
+                    int[] alter = [..s[0..i], ..s[(i+1)..]];
+                    if(IsSafe(alter))
+                    {
+                        sc++;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return sc.ToString();
+    }
+}
