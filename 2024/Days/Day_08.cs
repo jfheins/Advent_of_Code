0d6175c04fea08b42e2144633fbadf30917dc809
Julius Heins <Julius.JH.Heins@bmw.de>
2024-12-08 07:21:57 +0100

    Beautify

--- 2024/Days/Day_08.cs
+++ 2024/Days/Day_08.cs
@@ -1,130 +1,37 @@
-﻿using Core;
-using Spectre.Console;
-using System.Drawing;
+﻿using System.Drawing;
+using Core;
 using Core.Combinatorics;
-using Size = System.Drawing.Size;
 
 namespace AoC_2024.Days;
 
-public sealed partial class Day_08 : BaseDay
+public sealed class Day_08 : BaseDay
 {
-    private readonly string[] _input;
+    private readonly FiniteGrid2D<char> _grid;
 
     public Day_08()
     {
-        _input = File.ReadAllLines(InputFilePath);
+        var input = File.ReadAllLines(InputFilePath);
+        _grid = new FiniteGrid2D<char>(input);
     }
 
     public override async ValueTask<string> Solve_1()
-    {
-        var grid = new FiniteGrid2D<char>(_input);
-
-        var antennas = grid.Where(it => char.IsAsciiLetterOrDigit(it.value))
-            .ToLookup(it => it.value, it => it.pos);
-
-        HashSet<Point> antinodes = new();
-        foreach (var freq in antennas)
-        {
-            foreach (var other in grid.Keys)
-            {
-                if (freq.Contains(other))
-                    continue;
-                var dist = freq.SelectList(a => new Size(a.Minus(other)));
-                if (dist.Any(d => dist.Contains(2*d)))
-                {
-                    antinodes.Add(other);
-                    grid[other] = '#';
-                }
-            }
-        }
-        
-        return antinodes.Count.ToString();
-    }
+        => Solve(2, 1);
 
     public override async ValueTask<string> Solve_2()
+        => Solve(1, int.MaxValue);
+
+    private string Solve(int skip, int limit)
     {
-        var grid = new FiniteGrid2D<char>(_input);
-
-        var antennas = grid.Where(it => char.IsAsciiLetterOrDigit(it.value))
+        var antennas = _grid.Where(it => char.IsAsciiLetterOrDigit(it.value))
             .ToLookup(it => it.value, it => it.pos);
-
-        HashSet<Point> antinodes = new();
-        foreach (var freq in antennas)
-        {
-            var c = new TupleCombinations2<Point>(freq.ToList());
-
-            foreach (var tuple in c)
-            {
-                var delta = new Size(tuple.Item1.Minus(tuple.Item2));
-                foreach (var reso in grid.LineP(tuple.Item1, delta).Skip(1))
-                {
-                    antinodes.Add(reso);
-                    grid[reso] = '#';
-                }
-                foreach (var reso in grid.LineP(tuple.Item1, -1*delta).Skip(1))
-                {
-                    antinodes.Add(reso);
-                    grid[reso] = '#';
-                }
-                foreach (var reso in grid.LineP(tuple.Item2, delta).Skip(1))
-                {
-                    antinodes.Add(reso);
-                    grid[reso] = '#';
-                }
-                foreach (var reso in grid.LineP(tuple.Item2, -1*delta).Skip(1))
-                {
-                    antinodes.Add(reso);
-                    grid[reso] = '#';
-                }
-            }
-        }
-        
-        return antinodes.Count.ToString();
+        return antennas.SelectMany(freqBin => new TupleCombinations2<Point>([..freqBin]))
+            .SelectMany(tuple => GetAntiNodes(tuple, skip, limit)).ToHashSet().Count.ToString();
     }
 
-    private bool AreMultiplies(Size a, Size b)
+    private IEnumerable<Point> GetAntiNodes((Point, Point) pair, int skip, int limit)
     {
-        if (a.IsEmpty || b.IsEmpty)
-        {
-            return false;
-        }
-        
-        if (a.Width == 0 || b.Width == 0)
-        {
-            if (a.Width != 0 || b.Width != 0)
-            {
-                return false;
-            }
-
-            if (a.Height >= b.Height)
-            {
-                var scaling = Math.DivRem(a.Height, b.Height, out var remainder);
-                return a.Height == scaling * b.Height;
-            }
-            else
-            {
-                var scaling = Math.DivRem(b.Height, a.Height, out var remainder);
-                return b.Height == scaling * a.Height;
-            }
-        }
-        
-        if (a.Width >= b.Width)
-        {
-            var scaling = Math.DivRem(a.Width, b.Width, out var remainder);
-            if (remainder > 0)
-            {
-                return false;
-            }
-            return a.Height == scaling * b.Height;
-        }
-        else
-        {
-            var scaling = Math.DivRem(b.Width, a.Width, out var remainder);
-            if (remainder > 0)
-            {
-                return false;
-            }
-            return b.Height == scaling * a.Height;
-        }
+        var delta = new Size(pair.Item2.Minus(pair.Item1));
+        return _grid.LineP(pair.Item1, delta).Skip(skip).Take(limit)
+            .Concat(_grid.LineP(pair.Item2, -1 * delta).Skip(skip).Take(limit));
     }
 }
