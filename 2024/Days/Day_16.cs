cc790e5a5355432d9b8939d41ff65efbf92d8a78
Julius Heins <Julius.JH.Heins@bmw.de>
2024-12-16 08:09:57 +0100

    Solve day 16

--- 2024/Days/Day_16.cs
+++ 2024/Days/Day_16.cs
@@ -11,7 +11,7 @@ public sealed partial class Day_16 : BaseDay
     private int _maxCost;
     private FiniteGrid2D<char> _grid;
     private HashSet<(Point, Direction, int)> _pathPoints;
-    private HashSet<Point> _allPathPoints;
+    private HashSet<Point> _pathPoints2;
 
     public Day_16()
     {
@@ -32,6 +32,7 @@ public sealed partial class Day_16 : BaseDay
         var cost = 0;
         var prevDir = Direction.Right;
         _pathPoints = new();
+        _pathPoints2 = r.Steps.Select(it => it.p).ToHashSet();
         foreach (var tile in r.Steps.Skip(1))
         {
             cost += tile.d == prevDir ? 1 : 1000;
@@ -59,11 +60,13 @@ public sealed partial class Day_16 : BaseDay
         _grid = new FiniteGrid2D<char>(_input);
 
         var start = _grid.FindFirst('S');
+        var dest = _grid.FindFirst('E');
 
-        var d = new DijkstraSearch<(Point p, Direction d, int cost)>(null, Expander2);
-        var r = d.FindAll((start, Direction.Right, 0), it => _pathPoints.Contains((it.p, it.d, it.cost)));
-
-        var allPoints = r.SelectMany(it => it.Steps.Select(x => x.p)).ToHashSet();
+        var allPoints = new HashSet<Point>();
+        foreach (var po in _pathPoints2)
+        {
+            SimulateObstacle(start, dest, po, allPoints);
+        }
 
         foreach (var pp in allPoints)
         {
@@ -73,6 +76,64 @@ public sealed partial class Day_16 : BaseDay
         Console.WriteLine(_grid);
         
         return allPoints.Count.ToString();
+    }
+
+    private void SimulateObstacle(Point start, Point dest, Point po, HashSet<Point> allPoints)
+    {
+        var oldV = _grid[po];
+        _grid[po] = '#';
+        var d = new DijkstraSearch<(Point p, Direction d)>(null, Expander5);
+        var r = d.FindFirst((start, Direction.Right), it => it.p == dest);
+        var cost = (int)Math.Round(r?.Cost ?? 0);
+        
+        if (cost == _maxCost)
+        {
+            var newPath = r!.Steps.Select(it => it.p).ToHashSet();
+            newPath.ExceptWith(allPoints); // Find the detour
+            allPoints.UnionWith(newPath);
+            if (newPath.Count > 1)
+            {
+                foreach (var po2 in newPath)
+                {
+                    SimulateObstacle(start, dest, po2, allPoints);
+                }
+            }
+        }
+
+        _grid[po] = oldV;
+    }
+    
+    IEnumerable<((Point p, Direction d) node, float cost)> Expander5((Point p, Direction d) arg)
+    {
+        var nextPos = arg.p.MoveTo(arg.d);
+        if (_grid[nextPos] != '#')
+        {
+            yield return ((nextPos, arg.d), 1);
+        }
+
+        // turning 1000
+        yield return ((arg.p, arg.d.TurnClockwise()), 1000);
+        yield return ((arg.p, arg.d.TurnCounterClockwise()), 1000);
+    }
+    
+
+    private IEnumerable<(Point p, Direction d, int cost)> Expander3((Point p, Direction d, int cost) arg)
+    {
+        // turning 1000
+        if (arg.cost + 1000 <= _maxCost)
+        {
+            yield return (arg.p, arg.d.TurnClockwise(), arg.cost + 1000);
+            yield return (arg.p, arg.d.TurnCounterClockwise(), arg.cost + 1000);
+        }
+
+        if (arg.cost + 1 <= _maxCost)
+        {
+            var nextPos = arg.p.MoveTo(arg.d);
+            if (_grid[nextPos] != '#')
+            {
+                yield return (nextPos, arg.d, arg.cost + 1);
+            }
+        }
     }
 
     private IEnumerable<((Point p, Direction d, int cost) node, float cost)>
@@ -133,7 +194,8 @@ public sealed partial class Day_16 : BaseDay
     }
 }
 
-public class MyComparer : IEqualityComparer<(Point p, Direction d, ImmutableArray<int> cost)>
+public class MyComparer : IEqualityComparer<(Point p, Direction d, ImmutableArray<int> cost)>, 
+    IEqualityComparer<(Point p, Direction d, int cost)>
 {
     public bool Equals((Point p, Direction d, ImmutableArray<int> cost) x,
         (Point p, Direction d, ImmutableArray<int> cost) y)
@@ -146,5 +208,15 @@ public class MyComparer : IEqualityComparer<(Point p, Direction d, ImmutableArra
     public int GetHashCode((Point p, Direction d, ImmutableArray<int> cost) obj)
     {
         return HashCode.Combine(obj.Item1, (int)obj.Item2, obj.Item3.LastOrDefault());
+    }
+
+    public bool Equals((Point p, Direction d, int cost) x, (Point p, Direction d, int cost) y)
+    {
+        return x.p == y.p && x.d == y.d && x.cost == y.cost;
+    }
+
+    public int GetHashCode((Point p, Direction d, int cost) obj)
+    {
+        return HashCode.Combine(obj.Item1, (int)obj.Item2, obj.Item3);
     }
 }
