435f4d13e28dda93600012e59ce5a0ae2026c3e3
Julius F. Heins <jfheins@gmail.com>
2024-12-19 08:00:17 +0100

    Optimize

--- 2024/Days/Day_19.cs
+++ 2024/Days/Day_19.cs
@@ -15,20 +15,24 @@ public sealed class Day_19 : BaseDay
         _designs = input[1].ToArray();
     }
 
+    public override void Clear()
+    {
+        _matchCache.Clear();
+    }
+
     public override async ValueTask<string> Solve_1()
     {
-        _matchCache = new Dictionary<string, bool> { { "", true } };
+        _matchCache = new Dictionary<string, bool>(20_000) { { "", true } };
         var cache =_matchCache.GetAlternateLookup<ReadOnlySpan<char>>();
         return _designs.Count(design => IsMatch(design, cache)).ToString();
     }
 
     public override async ValueTask<string> Solve_2()
     {
-        var countCache = new Dictionary<string, long> { { "", 1 } };
+        var countCache = new Dictionary<string, long>(20_000) { { "", 1 } };
         var cache = countCache.GetAlternateLookup<ReadOnlySpan<char>>();
-        return _designs
-            .Where(it => _matchCache[it])
-            .Sum(design => CountMatches(design, cache)).ToString();
+        var cache2 = _matchCache.GetAlternateLookup<ReadOnlySpan<char>>();
+        return _designs.Sum(design => CountMatches(design, cache, cache2)).ToString();
     }
     
     private bool IsMatch(ReadOnlySpan<char> design, Dictionary<string, bool>.AlternateLookup<ReadOnlySpan<char>> cache)
@@ -45,18 +49,26 @@ public sealed class Day_19 : BaseDay
         return cache[design] = false;
     }
     
-    private long CountMatches(ReadOnlySpan<char> design, Dictionary<string, long>.AlternateLookup<ReadOnlySpan<char>> cache)
+    private long CountMatches(ReadOnlySpan<char> design,
+        Dictionary<string, long>.AlternateLookup<ReadOnlySpan<char>> cache,
+        Dictionary<string, bool>.AlternateLookup<ReadOnlySpan<char>> canMatchCache)
     {
         if (cache.ContainsKey(design))
             return cache[design];
+
+        if (IsDeadEnd(design))
+            return 0L;
 
         var matches = 0L;
         foreach (var t in _towels)
         {
             if (design.StartsWith(t)) 
-                matches += CountMatches(design[t.Length..], cache);
+                matches += CountMatches(design[t.Length..], cache, canMatchCache);
         }
-        cache[design] = matches;
-        return matches;
+        
+        return cache[design] = matches;
+        
+        bool IsDeadEnd(ReadOnlySpan<char> x)
+            => canMatchCache.TryGetValue(x, out var m) && !m;
     }
 }
