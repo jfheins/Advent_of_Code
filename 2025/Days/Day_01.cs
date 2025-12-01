ba279a5eb52f07536165f849d2f1b5f11a6062e2
Julius Heins <Julius.JH.Heins@bmw.de>
2025-12-01 15:08:28 +0100

    Beautify

--- 2025/Days/Day_01.cs
+++ 2025/Days/Day_01.cs
@@ -4,45 +4,44 @@ namespace AoC_2025.Days;
 
 public sealed class Day_01 : BaseDay
 {
-    private readonly string[] _input;
+    private readonly int[] _changes;
 
     public Day_01()
     {
-        _input = File.ReadAllLines(InputFilePath);
+        _changes = File.ReadAllLines(InputFilePath)
+            .SelectArray(ParseChange);
     }
 
     public override async ValueTask<string> Solve_1()
     {
-        return _input
-            .Select(ParseChange)
+        return _changes
             .RunningFold(50, (dial, change) => (dial + change).Modulo(100))
             .Count(dial => dial == 0)
             .ToString();
-
-        int ParseChange(string line)
-        {
-            var change = line.ParseInts(1)[0];
-            return change * (line.StartsWith('L') ? -1 : 1);
-        }
     }
 
     public override async ValueTask<string> Solve_2()
     {
-        var dial = 50;
-        var solution = 0;
-        foreach (var line in _input)
-        {
-            var change = line.ParseInts(1)[0];
-            var sign = line.StartsWith('L') ? -1 : 1;
+        return _changes
+            .Aggregate((dial: 50, zeroCrossings: 0), (state, change) =>
+            {
+                var sign = change < 0 ? -1 : 1;
+                var absChange = Math.Abs(change);
+                
+                // Mirror dial if turning left
+                var tempDial = (state.dial * sign).Modulo(100);
+                var fullTurns = Math.DivRem(tempDial + absChange, 100, out var newDial);
+                newDial = (newDial * sign).Modulo(100);
+                
+                return (dial: newDial, zeroCrossings: state.zeroCrossings + fullTurns);
+            })
+            .zeroCrossings
+            .ToString();
+    }
 
-            // Mirror dial if turning left
-            var tempDial = (dial * sign).Modulo(100);
-            var fullTurns = Math.DivRem(tempDial + change, 100, out var newDial);
-
-            solution += fullTurns;
-            dial = (newDial * sign).Modulo(100); // Mirror back
-        }
-
-        return solution.ToString();
+    private static int ParseChange(string line)
+    {
+        var change = int.Parse(line[1..]);
+        return change * (line.StartsWith('L') ? -1 : 1);
     }
 }
