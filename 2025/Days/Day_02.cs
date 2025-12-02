f08f5102214a00e44b9dc9ad73a172894410fc03
Julius Heins <Julius.JH.Heins@bmw.de>
2025-12-02 14:36:47 +0100

    Rename

--- 2025/Days/Day_02.cs
+++ 2025/Days/Day_02.cs
@@ -47,9 +47,9 @@ public sealed class Day_02 : BaseDay
         // A number that has 2 halves will be of the form xx * 101 or xxx * 1001 or xxxx * 10001 etc.
         // With xx being the prefix and 1001 is called magic number.
         // So the prefix needs to be half the number and the magic number has to divide id cleanly.
-        var prefixLength = id.DigitCount() / 2;
-        var magicNumber = (long)Math.Pow(10, prefixLength) + 1;
-        return id % magicNumber == 0 && (id / magicNumber).DigitCount() == prefixLength;
+        var patternLength = id.DigitCount() / 2;
+        var magicNumber = (long)Math.Pow(10, patternLength) + 1;
+        return id % magicNumber == 0 && (id / magicNumber).DigitCount() == patternLength;
     }
 
     private static readonly long[] MagicNumbers1 = [11, 111, 1111, 11111, 111111, 1111111, 11111111, 111111111, 1111111111];
@@ -73,22 +73,22 @@ public sealed class Day_02 : BaseDay
 
         return false;
 
-        bool Divides(long magicNumber, int prefixLength)
-            => id % magicNumber == 0 && (id / magicNumber).DigitCount() == prefixLength;
+        bool Divides(long magicNumber, int patternLength)
+            => id % magicNumber == 0 && (id / magicNumber).DigitCount() == patternLength;
     }
 
     private static bool IsInvalid2(ReadOnlySpan<char> strId)
     {
-        for (var prefixLen = 1; prefixLen <= strId.Length / 2; prefixLen++)
+        for (var patternLen = 1; patternLen <= strId.Length / 2; patternLen++)
         {
-            if (strId.Length % prefixLen > 0)
+            if (strId.Length % patternLen > 0)
                 continue;
-            var prefix = strId[..prefixLen];
+            var pattern = strId[..patternLen];
             var allMatch = true;
-            for (var i = 1; i < strId.Length / prefixLen; i++)
+            for (var i = 1; i < strId.Length / patternLen; i++)
             {
-                var part = strId[(prefixLen * i)..(prefixLen * (i + 1))];
-                if (!prefix.SequenceEqual(part))
+                var part = strId[(patternLen * i)..(patternLen * (i + 1))];
+                if (!pattern.SequenceEqual(part))
                 {
                     allMatch = false;
                     break;
