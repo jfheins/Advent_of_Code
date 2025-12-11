c5a435dfc067edaeb4b44fe9ff39514ce6ca2c9f
Julius Heins <Julius.JH.Heins@bmw.de>
2025-12-11 09:50:55 +0100

    Beautify

--- 2025/Days/Day_11.cs
+++ 2025/Days/Day_11.cs
@@ -4,63 +4,39 @@ namespace AoC_2025.Days;
 
 public sealed class Day_11 : BaseDay
 {
-    private readonly (string node, string[] outputs)[] _input;
+    private readonly Dictionary<(string from, string to), long> _cache = new(1200);
+    private readonly Dictionary<string, string[]> _graph;
 
     public Day_11()
     {
-        _input = File.ReadAllLines(InputFilePath).SelectArray(Parse);
-    }
-
-    private (string node, string[] outputs) Parse(string arg)
-    {
-        var x = arg.Split(':');
-        return (x[0], x[1].Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries));
+        var input = File.ReadAllLines(InputFilePath).SelectArray(Parse2);
+        _graph = input.ToDictionary(it => it[0], it => it[1..]);
+        
+        string[] Parse2(string it)
+            => it.Split([':', ' '], StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
     }
 
     public override async ValueTask<string> Solve_1()
-    {
-        var ex = _input.ToDictionary(it => it.node, it => it.outputs);
-        var paths = Search("you");
-
-        return paths.ToString();
-
-        int Search(string node)
-        {
-            if (node == "out")
-                return 1;
-
-            return ex.GetValueOrDefault(node, []).Select(Search).Sum();
-        }
-    }
+        => Search("you", "out").ToString();
 
     public override async ValueTask<string> Solve_2()
     {
-        var ex = _input.ToDictionary(it => it.node, it => it.outputs);
-        var cache = new Dictionary<(string from, string to), long>();
-        long part1;
-        var part2 = Search("dac", "fft");
-        long part3;
+        var passDacFirst = Search("dac", "fft") > 0;
 
-        if (part2 > 0)
-        {
-            part1 = Search("svr", "dac");
-            part3 = Search("fft", "out");
-            return (part1 * part2 * part3).ToString();
-        }
+        return passDacFirst
+            ? Search("svr", "dac", "fft", "out").ToString()
+            : Search("svr", "fft", "dac", "out").ToString();
+    }
 
-        part1 = Search("svr", "fft");
-        part2 = Search("fft", "dac");
-        part3 = Search("dac", "out");
+    private long Search(params IEnumerable<string> nodes)
+        => nodes.PairwiseWithOverlap().Select2(Search).Product();
 
-        return (part1 * part2 * part3).ToString();
+    private long Search(string node, string target)
+    {
+        if (node == target)
+            return 1;
 
-        long Search(string node, string target)
-        {
-            if (node == target)
-                return 1;
-
-            return cache.GetOrAdd((node, target),
-                _ => ex.GetValueOrDefault(node, []).Select(it => Search(it, target)).Sum());
-        }
+        return _cache.GetOrAdd((node, target),
+            _ => _graph.GetValueOrDefault(node, []).Select(it => Search(it, target)).Sum());
     }
 }
